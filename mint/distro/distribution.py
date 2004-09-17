82fdfe4a40ee83fdaa79576e48c8886fbbb09f67
dbc <devnull@localhost>
2004-09-17 00:02:54 +0000

    Empty changelog

--- mint/distro/distribution.py
+++ mint/distro/distribution.py
@@ -1,3 +1,4 @@
+import controlfile
 import files
 from lib import util
 from iso import ISO, DiskFullError
@@ -46,6 +47,8 @@ class Distribution:
         self.subdir = self.topdir + '/' + self.distro.productPath
         util.mkdirChain(os.path.join(self.subdir, 'changesets'))
         self.createChangeSets(os.path.join(self.subdir, 'changesets'), self.fromcspath)
+        from lib import epdb
+        epdb.set_trace()
         self.initializeCDs()
         self.writeCsList()
         self.makeInstRoots()
@@ -84,42 +87,17 @@ class Distribution:
                 
     def createChangeSets(self, csdir, fromcspath):
         self.csInfo = {}
-
         oldFiles = {}
         for path in [ "%s/%s" % (csdir, x) for x in os.listdir(csdir) ]:
             oldFiles[path] = 1
 
-        #'/data/test-buildsystem/buildroot/stage2/tmp/cs'
-        #csTrvList = trovelist.ChangeSetDirTroveList(fromcspath, self.cfg.installLabelPath[0], self.repos)
-        #csTrvList = trovelist.GroupTroveList('group-dist', self.cfg.installLabelPath[0], self.cfg.flavor, self.repos)
-        #controlFile = controlfile.ControlFile(repos, self.cfg)
-        #troveList = controlFile.getDesiredTroveList()
-
-        trvList = self.repos.findTrove(self.cfg.installLabelPath[0], "group-dist", self.cfg.flavor)
-        if not trvList:
-            print "no match for group-dist"
-            sys.exit(0)
-        elif len(trvList) > 1:
-            print "multiple matches for group-dist"
-            sys.exit(0)
-        groupTrv = trvList[0]
-        troves = {}
-        trovesByName = {}
-        for (name, version, flavor) in groupTrv.iterTroveList():
-            # XXX Hack until I've got flavors / group-dist:source hooked up
-            flavor = None
-            if pkg.name == "kernel" or pkg.name == 'kernel-source':
-                if "kernel.smp" not in str(pkg.flavor):
-                    flavor = deps.DependencySet()
-                    dep = deps.Dependency('use', ['!kernel.smp'])
-                    flavor.addDep(deps.UseDependency, dep)
-            pkg = PkgId(name, version, flavor, justName=True)
-            troves[pkg] = True
-            if name in trovesByName:
-                trovesByName[name].append(pkg)
-            else:
-                trovesByName[name] = [pkg]
-
+        control = controlfile.ControlFile(self.repos, self.cfg, self.cfg.installLabelPath[0]) 
+        control.loadControlFile()
+        print "Matching changesets..."
+        matches = control.getMatchedChangeSets(fromcspath)
+        
+        trovesByName = control.sources.copy()
+        l = len(trovesByName)
         list = []
         for name in [ 'setup', 'glibc' ]:
             for pkg in trovesByName[name]:
@@ -128,62 +106,35 @@ class Distribution:
 
         names = trovesByName.keys()
         names.sort()
-        for name in names:
-            for pkg in trovesByName[name]:
-                list.append(pkg)
-
-        l = len(list)
-        print "Creating %d packages" % len(list)
         index = 0
+        for name in names:
+            list.extend(trovesByName[name])
         for pkg in list:
             dispName = pkg.name
+            # XXX hack to add in flavor, since it is not 
+            # listed in group-dist
             if pkg.name == "kernel" or pkg.name == 'kernel-source':
                 if "!kernel.smp" not in str(pkg.flavor):
                     dispName += '-smp'
-
-            csfile = "%s-%s.ccs" % (dispName, pkg.version.trailingVersion().asString())
+                    flavor = deps.DependencySet()
+                    dep = deps.Dependency('use', ['!kernel.smp'])
+                    flavor.addDep(deps.UseDependency, dep)
+                    pkg = PkgId(dispNname, version, flavor, justName=True)
+            csfile = "%s-%s.ccs" % (pkg.name, pkg.version.trailingVersion().asString())
             path = "%s/%s" % (csdir, csfile)
 
             if oldFiles.has_key(path):
                 print >> sys.stderr, "%d/%d: keeping old %s" % (index, l, csfile)
                 del oldFiles[path]
+            elif pkg in matches:
+                # link the first matching path, assuming they are ordered
+                # so that latest is first
+                print >> sys.stderr, "%d/%d: linking %s" % (index, l, csfile)
+                os.link(os.path.join(fromcspath, matches[pkg][0][1]), path)
             else:
-                # XXX problem:
-                # Filenames / pkg strings could either contain 
-                # 1) all information about the cs
-                #    In this case we need to weed out on use-time
-                #    information that doesn't affect whether the cs
-                #    matches the group-dist specification
-                #    This makes the check to see if a package has already
-                #    been built much more than simply 
-                # 2) information about the cs that matches it to a
-                #    specific group-dist.  This is easier bc we know 
-                #    that information without needing to load the cs...
-                #    otherwise we might need to cook it to a temp file and
-                #    move it based on the cs's contents.
-                #
-                # group-dist contains information about *built packages*
-                # but, what we want is information about the *desired qualities*
-                # of pkgs...we could store all the information about the cs
-                # in its filename, or we could store the desired qualities.
-                # E.g. do we store the fact that this is the third rebuild
-                # of the changeset in the name?  
-                # XXX hack until file names match build system and group-dist
-                # Might have to load group-dist:source here to figure out 
-                # what matches watch
-                from lib import epdb
-                epdb.set_trace()
-                if dispName != 'kernel-smp':
-                    sourcepkg = 
-
-                frompath = os.path.join(fromcspath, pkg + '.ccs')
-                if os.path.exists(frompath):
-                    print >> sys.stderr, "%d/%d: linking %s" % (index, l, csfile)
-                    os.link(frompath, path)
-                else:
-                    print >> sys.stderr, "%d/%d: creating %s" % (index, l, csfile)
-                    self.repos.createChangeSetFile(
-                        [(pkg.name, (None, pkg.flavor), (pkg.version, pkg.flavor), True)], path)
+                print >> sys.stderr, "%d/%d: creating %s" % (index, l, csfile)
+                self.repos.createChangeSetFile(
+                    [(pkg.name, (None, pkg.flavor), (pkg.version, pkg.flavor), True)], path)
             cs = changeset.ChangeSetFromFile(path)
             pkgs = cs.primaryTroveList
             if len(pkgs) > 1:
@@ -197,13 +148,16 @@ class Distribution:
             version = '-'.join(v[:-2])
             release = '-'.join(v[-2:])
             size = 0
+            # calculate size when installed 
             for (fileId, (old, new, stream)) in cs.getFileList():
                 fileObj = files.ThawFile(stream, fileId)
                 if fileObj.hasContents:
                     size += fileObj.contents.size()
-            # size on installed machine
-            self.csInfo[pkg] = {'path': path, 'size': size, 'version' : version, 'release' : release}
+            self.csInfo[pkg] = {'path': path, 'size': size, 'version' : version, 
+                                'release' : release}
             index += 1
+
+
 
     def writeCsList(self):
         path = '/'.join((self.isos[0].builddir, self.distro.productPath, 'base/cslist'))
