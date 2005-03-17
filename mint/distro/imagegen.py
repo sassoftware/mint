19cb15cdbd2a366453a7a79c6b56c15e5988cc63
tgerla <devnull@localhost>
2005-03-17 20:01:53 +0000

    move grab/releaseOutput to correct class

--- mint/distro/imagegen.py
+++ mint/distro/imagegen.py
@@ -4,6 +4,9 @@
 # All Rights Reserved
 #
 
+import os
+import sys
+
 class ImageGenerator:
     def __init__(self, client, cfg, job, profileId):
         self.client = client
@@ -13,3 +16,15 @@ class ImageGenerator:
 
     def write(self):
         raise NotImplementedError
+
+    def grabOutput(self, logFile):
+        logfd = os.open(logFile, os.O_TRUNC | os.O_WRONLY | os.O_CREAT)
+        self.stdout = os.dup(sys.stdout.fileno())
+        self.stderr = os.dup(sys.stderr.fileno())
+        os.dup2(logfd, sys.stdout.fileno())
+        os.dup2(logfd, sys.stderr.fileno())
+        os.close(logfd)
+
+    def releaseOutput(self):
+        os.dup2(self.stdout, sys.stdout.fileno())
+        os.dup2(self.stderr, sys.stderr.fileno())
