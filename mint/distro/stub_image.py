8aaf22c9b0abfe384d117939d750144770cecc27
tgerla <devnull@localhost>
2005-05-06 17:56:22 +0000

    implement a simple stub image type for testing

---
+++ mint/distro/stub_image.py
@@ -0,0 +1,17 @@
+#
+# Copyright (c) 2005 rpath, Inc.
+#
+# All Rights Reserved
+#
+import os.path
+
+from imagegen import ImageGenerator
+
+class StubImage(ImageGenerator):
+    def write(self):
+        f = os.path.join(self.cfg.imagesPath, "stub.iso")
+
+        stub = file(f, "w")
+        print >> stub, "Hello World!"
+
+        return [f]
