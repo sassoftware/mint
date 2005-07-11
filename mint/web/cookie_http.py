f06fd8bfbab24c1e82a36f75a8c5ff7d96bfcf04
tgerla <devnull@localhost>
2005-07-11 23:09:13 +0000

    backport patch from WSZ branch

--- mint/web/cookie_http.py
+++ mint/web/cookie_http.py
@@ -18,6 +18,7 @@ from web import webauth
 from repository.netrepos import netserver
 
 class CookieHttpHandler(http.HttpHandler):
+
     def _requestAuth(self):
         return self._redirect("/login")
 
@@ -29,3 +30,28 @@ class CookieHttpHandler(http.HttpHandler):
             authToken = ('anonymous', 'anonymous')
             
         return authToken
+
+    # XXX: I'm wondering if this is the best approach.
+    # maybe it would be better to override _getHandler and
+    # provide a list of explicitly-allowed methods. opt-in
+    # instead of opt-out.
+    #
+    # remove some disallowed methods for mint:
+    del http.HttpHandler.main
+    del http.HttpHandler.metadata
+    del http.HttpHandler.chooseBranch
+    del http.HttpHandler.getMetadata
+    del http.HttpHandler.updateMetadata
+    del http.HttpHandler.userlist
+    del http.HttpHandler.addPermForm
+    del http.HttpHandler.addPerm
+    del http.HttpHandler.addGroupForm
+    del http.HttpHandler.manageGroupForm
+    del http.HttpHandler.manageGroup
+    del http.HttpHandler.addGroup
+    del http.HttpHandler.deleteGroup
+    del http.HttpHandler.deletePerm
+    del http.HttpHandler.addUser
+    del http.HttpHandler.deleteUser
+    del http.HttpHandler.chPassForm
+    del http.HttpHandler.chPass
