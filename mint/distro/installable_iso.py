0e0b77d9bf073875c2b0cd2280cd9559a7107c14
tgerla <devnull@localhost>
2005-08-31 16:51:38 +0000

    totally rework installable_iso.py to use new scripts to generate anaconda-installable ISOs

--- mint/distro/installable_iso.py
+++ mint/distro/installable_iso.py
@@ -3,34 +3,26 @@
 #
 # All Rights Reserved
 #
-import errno
 import os
+import re
 import sys
-import tempfile
 
+import conaryclient
 import deps
-import flavorcfg
-import repository
 import versions
 from build import use
-from conarycfg import ConfigFile, BOOLEAN
+from conarycfg import ConfigFile
+from lib import util
 
-import distribution 
 import conarycfg
 from mint.mint import upstream
 from imagegen import ImageGenerator
+import gencslist
 
 class IsoConfig(ConfigFile):
     defaults = {
-        'productPath':       'rpath',
-        'productName':       'rPath Linux',
-        'productPrefix':     'rpl',
-        'templatePath':      None,
-        'nfsPath':           None,
-        'tftpbootPath':      None,
-        'changesetCache':    None,
-        'instRootCache':     None,
-        'cachedAnaconda':    [ BOOLEAN, False ],
+        'imagesPath':   '/srv/mint/images/',
+        'scriptsPath':  '/srv/mint/code/scripts/',
     }
 
 class InstallableIso(ImageGenerator):
@@ -48,48 +40,109 @@ class InstallableIso(ImageGenerator):
         troveName, versionStr, flavorStr = release.getTrove()
         version = versions.ThawVersion(versionStr)
         flavor = deps.deps.ThawDependencySet(flavorStr)
-
         project = self.client.getProject(release.getProjectId())
 
-        ccfg = project.getConaryConfig()
+        cfg = conarycfg.ConaryConfiguration()
+        cfg.dbPath = ':memory:'
+        cfg.root = ':memory:'
+        cfg.initializeFlavors()
+        client = conaryclient.ConaryClient(cfg)
+        
+        topdir = os.path.join(isocfg.imagesPath, project.getHostname(), upstream(version), "unified") 
+        subdir = 'rPath'
+        
+        csdir = os.path.join(topdir, subdir, 'changesets')
 
-        flavorConfig = flavorcfg.FlavorConfig(ccfg.useDirs, ccfg.archDirs)
-        ccfg.flavor = flavorConfig.toDependency(override=ccfg.flavor[0])
-        insSet = deps.deps.DependencySet()
-        for dep in deps.arch.currentArch:
-            insSet.addDep(deps.deps.InstructionSetDependency, dep[0])
-        ccfg.flavor.union(insSet)
-        ccfg.buildFlavor = ccfg.flavor.copy()
-        flavorConfig.populateBuildFlags()
-       
-        repos = repository.netclient.NetworkRepositoryClient(ccfg.repositoryMap)
+        util.mkdirChain(csdir)
+        existingChangesets = {}
+        for path in [ "%s/%s" % (csdir, x) for x in os.listdir(csdir) ]:
+            existingChangesets[path] = 1
 
-        jobId = self.job.getId()
+        trvList = client.repos.findTrove(cfg.installLabelPath[0],\
+                                 (troveName, version.asString(), flavor),
+                                 defaultFlavor = cfg.flavor)
+
+        if not trvList:
+            print >> sys.stderr, "no match for", groupName
+            raise RuntimeException
+        elif len(trvList) > 1:
+            print >> sys.stderr, "multiple matches for", groupName
+            raise RuntimeException
+
+        groupName, groupVer, groupFlavor = trvList[0]
+        cslist = gencslist.processGroup(client, cfg, csdir, groupName,
+                                        groupVer, groupFlavor,
+                                        oldFiles = existingChangesets)
+
         releaseVer = upstream(version)
         releasePhase = "ALPHA"
-
         arch = release.getArch()
         assert(arch in ('x86', 'x86_64'))
+        if arch == 'x86':
+            anacondaArch = 'i386'
+        else:
+            anacondaArch = arch
  
-        distroInfo = distribution.DistroInfo(isocfg.productPrefix, isocfg.productPath, release.getName(),
-                                             releaseVer, releasePhase,
-                                             arch = arch,
-                                             isoSize = 620)
-       
-        tmpDir = self.cfg.imagesPath + os.path.join(arch, releasePhase)
-        dist = distribution.Distribution(arch, repos, ccfg,
-                                   distroInfo, (troveName, version.asString(), flavor),
-                                   buildpath = tmpDir, isopath = tmpDir+"/isos/",
-                                   isoTemplatePath = isocfg.templatePath,
-                                   nfspath = isocfg.nfsPath,
-                                   tftpbootpath = isocfg.tftpbootPath,
-                                   cachepath = isocfg.changesetCache,
-                                   instCachePath = isocfg.instRootCache,
-                                   cachedAnaconda = isocfg.cachedAnaconda,
-                                   statusCb = self.status,
-                                   clean = True)
-                                   
-        dist.prep()
-        filenames = dist.create()
+        cslistPath = os.path.join(topdir, subdir, 'base')
+        util.mkdirChain(cslistPath)
+        cslistFile = file(cslistPath + '/cslist', "w")
+        cslistFile.write("\n".join(cslist))
+        cslistFile.close()
+
+        infoMap = {
+            "isodir":       os.path.normpath(os.path.join(topdir, "..", "iso")),
+            "topdir":       topdir,
+            "subdir":       subdir,
+            "name":         project.getName(),
+            "safeName":     re.sub("\W", "_", project.getName()),
+            "version":      releaseVer,
+            "arch":         anacondaArch,
+            "scriptsdir":   isocfg.scriptsPath,
+        }
+        util.mkdirChain(infoMap['isodir'])
+
+        cmd = "%(scriptsdir)s/buildinstall --topdir %(topdir)s "\
+              "--subdir %(subdir)s --name \"%(name)s\" --version %(version)s "\
+              "--arch %(arch)s" % infoMap
+        print >> sys.stderr, cmd      
+        sys.stderr.flush()
+
+        os.system(cmd)
         
-        return filenames
+        cmd = "%(scriptsdir)s/splitdistro %(topdir)s" % infoMap
+        print >> sys.stderr, cmd
+        sys.stderr.flush()
+        os.system(cmd)
+        
+        isoList = []
+        isoname = "%(safeName)s-%(version)s-%%(disc)s.iso" % infoMap
+        discdir = os.path.normpath(topdir + "/../")
+        print >> sys.stderr, discdir, os.listdir(discdir)
+        for d in os.listdir(discdir):
+            if not d.startswith('disc'):
+                continue
+            
+            infoMap['disc'] = d
+            infoMap['discname'] = "%(name)s %(disc)s" % infoMap
+            infoMap['iso'] =  isoname % infoMap
+            if os.access(os.path.join(discdir, d, "isolinux/isolinux.bin"), os.R_OK):
+                os.chdir(os.path.join(discdir, d))
+                cmd = "mkisofs -o %(isodir)s/%(iso)s "\
+                              "-b isolinux/isolinux.bin "\
+                              "-c isolinux/boot.cat "\
+                              "-no-emul-boot "\
+                              "-boot-load-size 4 "\
+                              "-boot-info-table -R -J "\
+                              "-V \"%(discname)s\" -T ." % infoMap
+                print >> sys.stderr, cmd
+                sys.stderr.flush()
+                os.system(cmd)
+            else:
+                os.chdir(os.path.join(discdir, d))
+                cmd = "mkisofs -o %(isodir)s/%(iso)s -R -J -V \"%(discname)s\" -T ." % infoMap
+                print >> sys.stderr, cmd
+                sys.stderr.flush()
+                os.system(cmd)
+            isoList.append(infoMap['iso'])
+            
+        return [os.path.join(infoMap['isodir'], iso) for iso in isoList]
