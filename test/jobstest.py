4a67b676b1ab793da21aba8a36084990bfb05ad8
tgerla <devnull@localhost>
2005-09-19 06:59:10 +0000

    test duplicate job checking

--- test/jobstest.py
+++ test/jobstest.py
@@ -8,6 +8,7 @@ testsuite.setup()
 
 import rephelp
 from mint import jobstatus
+from mint import jobs
 
 class JobsTest(rephelp.RepositoryHelper):
     def testJobs(self):
@@ -17,13 +18,22 @@ class JobsTest(rephelp.RepositoryHelper):
         release = client.newRelease(projectId, "Test Release")
 
         job = client.startImageJob(release.getId())
-        jobs = list(client.iterJobs(releaseId = release.getId()))
-        assert(jobs[0].getReleaseId() == release.getId())
+        jobList = list(client.iterJobs(releaseId = release.getId()))
+        assert(jobList[0].getReleaseId() == release.getId())
 
+        # test restarting jobs
         job.setStatus(jobstatus.ERROR, "Error Message")
         client.startImageJob(release.getId())
 
         assert(job.getStatus() == jobstatus.WAITING)
 
+        # test duplicate job handling
+        try:
+            job2 = client.startImageJob(release.getId())
+        except jobs.DuplicateJob:
+            pass
+        else:
+            self.fail("expected exception jobs.DuplicateJob")
+        
 if __name__ == "__main__":
     testsuite.main()
