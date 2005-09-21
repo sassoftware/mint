7821da0ba1f7c7e894386bd2143a40f1feb2b9ec
tgerla <devnull@localhost>
2005-09-21 22:06:14 +0000

    merge to latest conary-test rephelp.py and testsuite.py; move some Mint functionality into mint_rephelp.py MintRepositoryHelper class; add repostest.py to test mint-created repositories

--- test/repostest.py
+++ test/repostest.py
@@ -14,28 +14,6 @@ from mint import userlevels
 from lib import openpgpkey
 
 class RepositoryTest(MintRepositoryHelper):
-    def newProject(self, client, name = "Test Project",
-                         hostname = "test",
-                         domainname = "localhost",
-                         username = "testuser"):
-        # save the current openpgpkey cache
-        keyCache = openpgpkey.getKeyCache()
-        projectId = client.newProject(name, hostname, domainname)
-
-        # set a default signature key
-        project = client.getProject(projectId)
-        ascKey = open(testsuite.archivePath + '/key.asc', 'r').read()
-        project.addUserKey(username, ascKey)
-
-        # restore the key cache
-        openpgpkey.setKeyCache(keyCache)
-
-        self.cfg.buildLabel = versions.Label("%s.%s@rpl:devel" % (hostname, domainname))
-        self.cfg.repositoryMap = {"%s.%s" % (hostname, domainname):
-            "http://testuser:testpass@%s:%d/repos/%s/" % (domainname, self.getPort(), hostname)}
-        
-        return projectId
-
     def testBasicRepository(self):
         client = self.getMintClient("testuser", "testpass")
         projectId = self.newProject(client)
