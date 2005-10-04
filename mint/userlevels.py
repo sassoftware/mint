037fceb653859892381e1740f14da74eac405973
jtate <devnull@localhost>
2005-10-04 20:38:37 +0000

    Add USER userlevel

--- mint/userlevels.py
+++ mint/userlevels.py
@@ -4,12 +4,20 @@
 # All Rights Reserved
 #
 
-OWNER, DEVELOPER = range(0, 2)
-LEVELS = range(0, 2)
+#LEVELS may only be appended at the end!  Numeric values are stored in the DB.  
+#Any discrepency could result in a security issue.
+NONMEMBER=-1
+OWNER, DEVELOPER, USER = range(0, 3)
+
+#This list is sorted in order of permissions
+WRITERS = [OWNER, DEVELOPER]
+READERS = [USER]
+LEVELS = WRITERS + READERS
+
 
 names = {
-    OWNER: "Owner",
-    DEVELOPER: "Developer",
+    OWNER:      "Owner",
+    DEVELOPER:  "Developer",
+    USER:       "User",
 }
 
-NONMEMBER=-1
