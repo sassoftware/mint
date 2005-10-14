81bf4953623ce66926c2c3c6df7e60b1adcfc2d7
tgerla <devnull@localhost>
2005-10-14 01:58:18 +0000

    cast disk num to int

--- mint/distro/installable_iso.py
+++ mint/distro/installable_iso.py
@@ -270,7 +270,7 @@ class InstallableIso(ImageGenerator):
                 continue
             
             discNum = d.split("disc")[-1]
-            d = "Disk %d" % discNum
+            d = "Disk %d" % int(discNum)
             truncatedName = infoMap['name'][:31-len(d)]
             infoMap['discname'] = "%s %s" % (truncatedName, d)
             infoMap['iso'] =  isoname % infoMap
