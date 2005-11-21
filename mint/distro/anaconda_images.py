db27ecf13724d7f67d0c0cf6dd936d4ba33e4117
Tim Gerla <http://bugs.rpath.com/>
2005-11-21 15:30:35 -0400

    correct anaconda image filenames

--- mint/distro/anaconda_images.py
+++ mint/distro/anaconda_images.py
@@ -22,11 +22,11 @@ class AnacondaImages:
 
     images = [
          #filename                  resolution      Beta?   position    size    template
-        ["anaconda-header.png",     (800,58),       False,  (10,3),     30,     "background-800x58.png"],
+        ["anaconda_header.png",     (800,58),       False,  (10,3),     30,     "background-800x58.png"],
         ["first-lowres.png",        (640,480),      True,   0.80,       40,     "background-640x480.png"],
         ["first.png",               (800,600),      True,   0.80,       40,     "background-800x600.png"],
-        ["progress-first-375.png",  (353,170),      False,  0.80,       20,     "background-353x170.png"],
-        ["progress-first.png",      (500,325),      False,  0.80,       40,     "background-500x325.png"],
+        ["progress_first-375.png",  (353,170),      False,  0.80,       20,     "background-353x170.png"],
+        ["progress_first.png",      (500,325),      False,  0.80,       40,     "background-500x325.png"],
         ["splash.png",              (400,420),      True,   0.33,       40,     "background-400x420.png"],
         #["syslinux-splash.png",     (640,300),      True,   (10,140),   20,     "background-640x300.png"]
     ]
