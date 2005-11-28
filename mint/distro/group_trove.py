78554e8d0fe97d786e67111d6810c89c9c5cc22d
Tim Gerla <http://bugs.rpath.com/>
2005-11-28 14:06:26 -0400

    add reasonable stock flavors for various architectures, and cook using those

--- mint/distro/group_trove.py
+++ mint/distro/group_trove.py
@@ -17,6 +17,21 @@ from conary.repository import netclient
 from imagegen import ImageGenerator
 from mint import projects
 
+stockFlavors = {
+    "1#x86":    "~X,~!alternatives,!bootstrap,~builddocs,~buildtests,"
+                "~desktop,~dietlibc,~emacs,~gcj,~gnome,~gtk,~ipv6,~kde,"
+                "!kernel.smp,~krb,~ldap,~nptl,pam,~pcre,~perl,~!pie,"
+                "~python,~qt,~readline,~!sasl,~!selinux,ssl,~tcl,"
+                "tcpwrappers,~tk,~!xfce is: x86(~cmov,~i486,~i586,~i686,~mmx,~sse,~sse2)",
+
+    "1#x86_64": "~X,~!alternatives,!bootstrap,~builddocs,"
+                "~buildtests,~desktop,!dietlibc,~emacs,"
+                "~gcj,~gnome,~gtk,~ipv6,~kde,~krb,~ldap,"
+                "~nptl,pam,~pcre,~perl,~!pie,~python,~qt,"
+                "~readline,~!sasl,~!selinux,ssl,~tcl,"
+                "tcpwrappers,~tk,~!xfce is: x86_64(~3dnow,~3dnowext,~nx)",
+}
+
 class GroupTroveCook(ImageGenerator):
     def write(self):
         self.status("Cooking group")
@@ -39,7 +54,7 @@ class GroupTroveCook(ImageGenerator):
             cfg.contact = "http://www.rpath.org"
             cfg.quiet = True
             cfg.buildLabel = versions.Label(project.getLabel())
-            cfg.buildFlavor = arch
+            cfg.buildFlavor = stockFlavors[arch]
             cfg.initializeFlavors()
             cfg.repositoryMap = project.getConaryConfig().repositoryMap
             
