2b64e8ee92899d0717b00a68cde0b00e4bd07a8d
Tim Gerla <http://bugs.rpath.com/>
2005-12-08 17:52:07 -0400

    default to using SSL

--- mint/distro/group_trove.py
+++ mint/distro/group_trove.py
@@ -49,7 +49,7 @@ class GroupTroveCook(ImageGenerator):
 
             project = self.client.getProject(projectId)
 
-            cfg = project.getConaryConfig()
+            cfg = project.getConaryConfig(overrideSSL=True, useSSL=True)
             cfg.name = "rBuilder Online"
             cfg.contact = "http://www.rpath.org"
             cfg.quiet = True
