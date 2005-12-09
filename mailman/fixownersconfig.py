d2c7a56342e98f43f821543f6fea784f3e9b6ec9
Tim Gerla <http://bugs.rpath.com/>
2005-12-09 17:22:43 -0400

    switch to new config class layout

--- mailman/fixownersconfig.py
+++ mailman/fixownersconfig.py
@@ -4,11 +4,9 @@ from conary.conarycfg import ConfigFile
 import urllib
 
 class FixOwnersConfig(ConfigFile):
-    defaults = {
-        'privateUrlBase'        : 'http://%%s:%%s@mint.rpath.org/xmlrpc-private',
-        'authUser'          : 'mintauth',
-        'authPass'          : 'mintpass',
-    }
+    privateUrlBase = 'http://%%s:%%s@mint.rpath.org/xmlrpc-private'
+    authUser       = 'mintauth'
+    authPass       = 'mintpass'
 
     def read(self, path, exception = False):
         ConfigFile.read(self, path, exception)
