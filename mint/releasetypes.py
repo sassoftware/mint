c9b7e31a007a6d8e76d075ba38181a07da29b578
Stu Gott <http://bugs.rpath.com/>
2006-01-12 16:54:49 -0400

    tweak releasetypes

--- mint/releasetypes.py
+++ mint/releasetypes.py
@@ -4,11 +4,12 @@
 # All Rights Reserved
 #
 
-BOOTABLE_IMAGE, INSTALLABLE_ISO, STUB_IMAGE, LIVE_CF_IMAGE, NETBOOT_IMAGE, GROUP_TROVE_COOK, LIVE_ISO, QEMU_IMAGE, VMWARE_IMAGE = range(0, 9)
 TYPES = range(0, 9)
+BOOTABLE_IMAGE, INSTALLABLE_ISO, STUB_IMAGE, LIVE_CF_IMAGE, NETBOOT_IMAGE, GROUP_TROVE_COOK, LIVE_ISO, QEMU_IMAGE, VMWARE_IMAGE = TYPES
+
 
 #This array contains the imageTypes that are to be displayed on the release creation page
-visibleImageTypes = [ INSTALLABLE_ISO, QEMU_IMAGE, VMWARE_IMAGE ]
+visibleImageTypes = ( INSTALLABLE_ISO, QEMU_IMAGE, VMWARE_IMAGE )
 
 #BOOTABLE_IMAGE Should never get stored in the DB and therefore doesn't need a name
 typeNames = {
