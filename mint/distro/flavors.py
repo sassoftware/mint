2544eef5917072df32d9e0d366d764f0fff2c227
Tim Gerla <http://bugs.rpath.com/>
2006-02-02 17:01:07 -0500

    update stock group flavors to something recent

--- mint/distro/flavors.py
+++ mint/distro/flavors.py
@@ -4,16 +4,24 @@
 #
 
 stockFlavors = {
-    "1#x86":    "~X,~!alternatives,!bootstrap,~builddocs,~buildtests,"
-                "~desktop,~dietlibc,~emacs,~gcj,~gnome,~gtk,~ipv6,~kde,"
-                "!kernel.smp,~krb,~ldap,~nptl,pam,~pcre,~perl,~!pie,"
-                "~python,~qt,~readline,~!sasl,~!selinux,ssl,~tcl,"
-                "tcpwrappers,~tk,~!xfce is: x86(~cmov,~i486,~i586,~i686,~mmx,~sse,~sse2)",
-
-    "1#x86_64": "~X,~!alternatives,!bootstrap,~builddocs,"
-                "~buildtests,~desktop,!dietlibc,~emacs,"
-                "~gcj,~gnome,~gtk,~ipv6,~kde,~krb,~ldap,"
-                "~nptl,pam,~pcre,~perl,~!pie,~python,~qt,"
-                "~readline,~!sasl,~!selinux,ssl,~tcl,"
-                "tcpwrappers,~tk,~!xfce is: x86_64(~3dnow,~3dnowext,~nx)",
+    "1#x86":    "~MySQL-python.threadsafe, X, ~!alternatives, ~!bootstrap,"
+                "~!builddocs, ~buildtests, desktop, dietlibc, emacs, gcj,"
+                "~glibc.tls, gnome, ~!grub.static, gtk, ipv6, kde,"
+                "~!kernel.debug, ~!kernel.debugdata, ~!kernel.numa,"
+                "~kernel.smp, krb, ldap, nptl, ~!openssh.smartcard,"
+                "~!openssh.static_libcrypto, pam, pcre, perl, ~!pie,"
+                "~!postfix.mysql, python, qt, readline, sasl, ~!selinux,"
+                "~sqlite.threadsafe, ssl, tcl, tcpwrappers, tk, ~!xorg-"
+                "x11.xprint is: x86(cmov, i486, i586, i686, ~!mmx, ~!sse2)",
+    "1#x86_64":
+                "~MySQL-python.threadsafe, X, ~!alternatives, ~!bootstrap,"
+                "~!builddocs, ~buildtests, desktop, ~!dietlibc, emacs, gcj,"
+                "~glibc.tls, gnome, ~grub.static, gtk, ipv6, kde,"
+                "~!kernel.debug, ~!kernel.debugdata, ~!kernel.numa, krb, ldap,"
+                "nptl, ~!openssh.smartcard, ~!openssh.static_libcrypto, pam,"
+                "pcre, perl, ~!pie, ~!postfix.mysql, python, qt, readline,"
+                "sasl, ~!selinux, ~sqlite.threadsafe, ssl, tcl, tcpwrappers,"
+                "tk, ~!xorg-x11.xprint is: x86(i486, i586, i686, ~!sse2)"
+                "x86_64",
 }
+
