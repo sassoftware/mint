9ccc2330e4c0c9e5c16821813134da51605db31d
Tim Gerla <http://bugs.rpath.com/>
2006-02-02 17:52:49 -0500

    correct x86_64 flavor

--- mint/distro/flavors.py
+++ mint/distro/flavors.py
@@ -14,14 +14,12 @@ stockFlavors = {
                 "~sqlite.threadsafe, ssl, tcl, tcpwrappers, tk, ~!xorg-"
                 "x11.xprint is: x86(cmov, i486, i586, i686, ~!mmx, ~!sse2)",
     "1#x86_64":
-                "~MySQL-python.threadsafe, X, ~!alternatives, ~!bootstrap,"
-                "~!builddocs, ~buildtests, desktop, ~!dietlibc, emacs, gcj,"
-                "~glibc.tls, gnome, ~grub.static, gtk, ipv6, kde,"
-                "~!kernel.debug, ~!kernel.debugdata, ~!kernel.numa, krb, ldap,"
-                "nptl, ~!openssh.smartcard, ~!openssh.static_libcrypto, pam,"
-                "pcre, perl, ~!pie, ~!postfix.mysql, python, qt, readline,"
-                "sasl, ~!selinux, ~sqlite.threadsafe, ssl, tcl, tcpwrappers,"
-                "tk, ~!xorg-x11.xprint is: x86(i486, i586, i686, ~!sse2)"
-                "x86_64",
+                "~MySQL-python.threadsafe,X,~!alternatives,~!bootstrap,"
+                "~!builddocs,~buildtests,desktop,~!dietlibc,emacs,gcj,"
+                "~glibc.tls,gnome,~grub.static,gtk,ipv6,kde,~!kernel.debug,"
+                "~!kernel.debugdata,~!kernel.numa,krb,ldap,nptl,"
+                "~!openssh.smartcard,~!openssh.static_libcrypto,pam,pcre,"
+                "perl,~!pie,~!postfix.mysql,python,qt,readline,sasl,"
+                "~!selinux,~sqlite.threadsafe,ssl,tcl,tcpwrappers,tk,"
+                "~!xorg-x11.xprint is: x86(i486,i586,i686,~!sse2) x86_64",
 }
-
