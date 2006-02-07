af61688f7e9e8488f354c60bce4627e35adab032
Stu Gott <http://bugs.rpath.com/>
2006-02-07 15:48:41 -0500

    add file to define cook types in the future

---
+++ mint/cooktypes.py
@@ -0,0 +1,18 @@
+#
+# Copyright (c) 2005-2006 rPath, Inc.
+#
+# All Rights Reserved
+#
+
+import sys
+
+validCookTypes = {
+    'DUMMY_COOK'    : 0,
+    'GROUP_BUILDER' : 1
+    }
+
+TYPES = validCookTypes.values()
+
+# add all the defined image types directly to the module so that the standard
+# approach of "cooktypes.COOK_TYPE" will result in the expected enum
+sys.modules[__name__].__dict__.update(validCookTypes)
