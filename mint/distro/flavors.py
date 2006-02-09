bd951253ca00157d04a72a2543b972241f859008
Tim Gerla <http://bugs.rpath.com/>
2006-02-09 16:46:29 -0500

    fix up group flavors to not require all CPU modifiers

--- mint/distro/flavors.py
+++ mint/distro/flavors.py
@@ -12,7 +12,7 @@ stockFlavors = {
         "nptl, ~!openssh.smartcard, ~!openssh.static_libcrypto, pam,"
         "pcre, perl, ~!pie, ~!postfix.mysql, python, qt, readline,"
         "sasl, ~!selinux, ~sqlite.threadsafe, ssl, tcl, tcpwrappers,"
-        "tk, ~!xorg-x11.xprint is: x86(cmov, i486, i586, i686, ~!mmx,"
+        "tk, ~!xorg-x11.xprint is: x86(~cmov, ~i486, ~i586, ~i686, ~!mmx,"
         "~!sse2)",
 
     "1#x86_64":
@@ -23,5 +23,5 @@ stockFlavors = {
                 "~!openssh.smartcard,~!openssh.static_libcrypto,pam,pcre,"
                 "perl,~!pie,~!postfix.mysql,python,qt,readline,sasl,"
                 "~!selinux,~sqlite.threadsafe,ssl,tcl,tcpwrappers,tk,"
-                "~!xorg-x11.xprint is: x86(i486,i586,i686,~!sse2) x86_64",
+                "~!xorg-x11.xprint is: x86(~i486,~i586,~i686,~!sse2) x86_64",
 }
