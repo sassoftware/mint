335c69ed73b3f8830d161136537d9bbbcc6b0053
Tim Gerla <http://bugs.rpath.com/>
2006-03-01 10:29:17 -0500

    more terminology normalization

--- mint/releases.py
+++ mint/releases.py
@@ -78,7 +78,7 @@ stubImageTemplate = {
 dataHeadings = {
     releasetypes.BOOTABLE_IMAGE   : 'Image Settings',
     releasetypes.INSTALLABLE_ISO  : 'Installable CD Settings',
-    releasetypes.QEMU_IMAGE       : 'Raw HDD Image Settings',
+    releasetypes.QEMU_IMAGE       : 'Raw Hard Disk Image Settings',
     releasetypes.VMWARE_IMAGE     : 'VMware Image Settings',
     releasetypes.STUB_IMAGE       : 'Stub Image Settings',
 }
