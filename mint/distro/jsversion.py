00029c6b26314414495d04cf3fa1d9b5d87ed9ae
Stu Gott <http://bugs.rpath.com/>
2006-03-15 18:06:21 -0500

    dynamically report job server versions available.

--- mint/distro/jsversion.py
+++ mint/distro/jsversion.py
@@ -3,8 +3,36 @@
 #
 # All Rights Reserved
 #
-def getVersions():
-    return ['1.5.4']
+import os
+import re
+from mint import mint_error
 
-def getDefaultVersion():
-    return getVersions()[-1]
+from conary.conaryclient.cmdline import parseTroveSpec
+
+DEFAULT_BASEPATH = os.path.join(os.path.sep, 'srv', 'mint', 'jobserver')
+
+def _getVersions(basePath = None):
+    if basePath is None:
+        basePath = DEFAULT_BASEPATH
+    return sorted([x for x in os.listdir(basePath) \
+                   if re.match('\d*(\.\d*)+.*', x) \
+                   and os.path.isdir(os.path.join(basePath, x))])
+
+def getVersions(basePath = None):
+    if basePath is None:
+        basePath = DEFAULT_BASEPATH
+    try:
+        f = open(os.path.join(basePath, 'versions'))
+    except:
+        return _getVersions(basePath)
+    try:
+        return [parseTroveSpec(x)[1].split('-')[0] for x in f.readlines()]
+    finally:
+        f.close()
+
+def getDefaultVersion(basePath = None):
+    try:
+        return getVersions(basePath)[-1]
+    except IndexError:
+        raise mint_error.JobserverVersionMismatch("No job servers could be found.")
+
