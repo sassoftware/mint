1436510369ea761f6e14b6146c1c5c3041301ac3
Stu Gott <http://bugs.rpath.com/>
2006-03-15 18:09:29 -0500

    more rigorous control of job server.

--- test/jobserverharness.py
+++ test/jobserverharness.py
@@ -30,7 +30,10 @@ class JobServerHelper(MintRepositoryHelper):
             pid = -1
             while tries < 10:
                 if os.path.exists(lockFile):
-                    pid = int(open(lockFile).read())
+                    try:
+                        pid = int(open(lockFile).read())
+                    except ValueError:
+                        self.fail("pid file belongs to another job server?")
                     break
                 tries += 1
                 time.sleep(0.5)
@@ -41,6 +44,19 @@ class JobServerHelper(MintRepositoryHelper):
         os.system("../scripts/job-server --config=%s -k" % (self.tmpDir + "/iso_gen.conf"))
 
         os.waitpid(self.jsPid, 0)
+        # wait for a pid file to disappear
+        lockFile = os.path.join(self.reposDir, "jobserver", "jobserver.pid")
+        tries = 0
+        success = False
+        # it can take up to 15 seconds, so this needs to be generous.
+        while tries < 30:
+            if not os.path.exists(lockFile):
+                success = True
+                break
+            tries += 1
+            time.sleep(1)
+        if not success:
+            self.fail("Job server didn't shut down.")
 
     def setUp(self):
         MintRepositoryHelper.setUp(self)
