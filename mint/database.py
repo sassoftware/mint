00fa4fd8fbe83175880a727d61f167b93486edaf
Stu Gott <http://bugs.rpath.com/>
2006-06-02 14:29:32 -0400

    bump schema version

--- mint/database.py
+++ mint/database.py
@@ -84,7 +84,7 @@ class DatabaseTable(object):
     those indeces
     """
 
-    schemaVersion = 18
+    schemaVersion = 19
     name = "Table"
     fields = []
     createSQL = "CREATE TABLE Table ();"
