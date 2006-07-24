12485ca6e35d7a799fb7342a12a9652db950eb3e
Tim Gerla <http://issues.rpath.com/>
2006-07-24 17:00:05 -0400

    pass the function, not the result of the function

--- mint/web/content/javascript/trovepicker.js
+++ mint/web/content/javascript/trovepicker.js
@@ -54,7 +54,7 @@ function TrovePicker(projectId, serverName, troveName, pickerId, mintStaticPath,
         this.allowNameChoice = false;
     }
 
-    addLoadEvent(this.buildTrovePicker());
+    addLoadEvent(this.buildTrovePicker);
 }
 
 TrovePicker.prototype.troveName = null;
