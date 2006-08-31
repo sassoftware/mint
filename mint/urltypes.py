19ebcb6dbf4bdeacf211690bf03feaf42ba2aaba
Jason Loomis <http://bugs.rpath.com>
2006-08-31 18:10:41 -0400

    added constants for external images

---
+++ mint/urltypes.py
@@ -0,0 +1,31 @@
+#
+# Copyright (c) 2005-2006 rPath, Inc.
+#
+# All Rights Reserved
+#
+
+import sys
+
+urlTypes = {
+    'LOCAL'           : 0,
+    'AMAZONS3'        : 1,
+    'AMAZONS3TORRENT' : 2
+    }
+
+TYPES = urlTypes.values()
+
+# add all the defined image types directly to the module so that the standard
+# approach of "urltypes.URL_TYPE" will result in the expected enum
+sys.modules[__name__].__dict__.update(urlTypes)
+
+typeNames = {
+    LOCAL           : 'Locally Stored',
+    AMAZONS3        : 'Amazon S3',
+    AMAZONS3TORRENT : 'Amazon S3 BitTorrent'
+    }
+
+displayNames = {
+    LOCAL           : '',
+    AMAZONS3        : '',
+    AMAZONS3TORRENT : 'BitTorrent'
+}
