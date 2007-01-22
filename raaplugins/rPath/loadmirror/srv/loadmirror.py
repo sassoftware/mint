be0c6ac8919f5ec455e36256f30580eca64498e1
Jason Loomis <http://bugs.rpath.com>
2007-01-22 11:41:42 -0500

    move disk space check out of _startPreload

--- raaplugins/rPath/loadmirror/srv/loadmirror.py
+++ raaplugins/rPath/loadmirror/srv/loadmirror.py
@@ -28,6 +28,14 @@ class LoadMirror(rAASrvPlugin):
                 loadmirror.mountMirrorLoadDrive()
             except loadmirror.NoMirrorLoadDiskFound, e:
                 error = str(e)
+            srv = os.statvfs('/srv')
+            disk = os.statvfs(loadmirror.target)
+            available = srv[statvfs.F_BSIZE] * srv[statvfs.F_BAVAIL]
+            repoSize = int(disk[statvfs.F_BSIZE] * (disk[statvfs.F_BLOCKS] - disk[statvfs.F_BFREE]) * 1.1)
+            if available < repoSize:
+                error = '%s MB of disk space needed to preload mirror but only %s MB available' % (repoSize / 0x100000, available / 0x100000)
+                log = loadmirror.logger
+                log.error(error)
         elif cmd == "preload":
             self._startPreload()
 
@@ -41,17 +49,9 @@ class LoadMirror(rAASrvPlugin):
 
         log = loadmirror.logger
         loader = loadmirror.LoadMirror(loadmirror.target,
-            'http://%s:%s@localhost/xmlrpc-private/' % (cfg.authUser, cfg.authPass))
+            'http://%s:%s@mint.rpath.local/xmlrpc-private/' % (cfg.authUser, cfg.authPass))
         loadmirror.mountMirrorLoadDrive()
         
-        srv = os.statvfs('/srv')
-        disk = os.statvfs(loadmirror.target)
-        available = srv[statvfs.F_BSIZE] * srv[statvfs.F_BAVAIL]
-        repoSize = int(disk[statvfs.F_BSIZE] * (disk[statvfs.F_BLOCKS] - disk[statvfs.F_BAVAIL]) * 1.1)
-        if available < repoSize:
-            error = '%s MB of disk space needed to preload mirror but only %s MB available' % (repoSize / 0x100000, available / 0x100000)
-            log.error(error)
-            raise error
         
         mirrored = 0
         for serverName in os.listdir(loadmirror.target):
