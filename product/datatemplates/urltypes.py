f994633236f5a97a23ca080be2be2ea54c59a902
Tim Gerla <http://issues.rpath.com/>
2007-02-07 12:57:24 -0500

    overlay a custom urltypes.py for product build

---
+++ product/datatemplates/urltypes.py
@@ -0,0 +1,34 @@
+#
+# Copyright (c) 2005-2007 rPath, Inc.
+#
+# All Rights Reserved
+#
+
+import sys
+
+urlTypes = {
+    'LOCAL'           : 0,
+    'AMAZONS3'        : 1,
+    'AMAZONS3TORRENT' : 2,
+    'GENERICMIRROR'   : 999
+    }
+
+TYPES = urlTypes.values()
+
+# add all the defined image types directly to the module so that the standard
+# approach of "urltypes.URL_TYPE" will result in the expected enum
+sys.modules[__name__].__dict__.update(urlTypes)
+
+typeNames = {
+    LOCAL           : 'Locally Stored',
+    AMAZONS3        : 'Amazon S3',
+    AMAZONS3TORRENT : 'Amazon S3 BitTorrent',
+    GENERICMIRROR   : 'Generic Mirror Site'
+    }
+
+displayNames = {
+    LOCAL           : 'Download',
+    AMAZONS3        : 'Download',
+    AMAZONS3TORRENT : 'Download (BitTorrent)',
+    GENERICMIRROR   : 'Download'
+}
