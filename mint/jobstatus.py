7512910ec2f159b150f2336a403920f8a53763e3
Stu Gott <http://issues.rpath.com/>
2007-03-13 12:29:58 -0400

    use new status codes

--- mint/jobstatus.py
+++ mint/jobstatus.py
@@ -4,18 +4,27 @@
 # All Rights Reserved
 #
 
-UNKNOWN, WAITING, RUNNING, FINISHED, DELETED, ERROR, NOJOB = range(-1, 6)
+import sys
 
-STATUSES = range(-1, 6)
-statusNames = {
-    UNKNOWN: "Unknown",
-    WAITING: "Waiting",
-    RUNNING: "Running",
-    FINISHED: "Finished",
-    DELETED: "Deleted",
-    ERROR: "Error",
-    NOJOB: "No Job"
-}
+statuses = {
+    'UNKNOWN'  : -1,
+    'WAITING'  : 0,
+    'RUNNING'  : 100,
+    'BUILT'    : 200,
+    'FINISHED' : 300,
+    'FAILED'   : 301,
+    'ERROR'    : 301,
+    'NO_JOB'   : 401
+    }
+
+sys.modules[__name__].__dict__.update(statuses)
+statusNames = dict([(statuses[x[0]], x[0].capitalize().replace('_', ' ')) \
+                        for x in statuses.iteritems()])
+
+stautsCodeNames = dict([(x[1], 'STATUS_' + x[0].replace('_', '')) \
+                            for x in statuses.iteritems()])
+
+STATUSES = statuses.keys()
 
 statusCodeNames = {
     UNKNOWN:  "STATUS_UNKNOWN",
