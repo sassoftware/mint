5d5832d306a3ee962a99bc468b0f592bf4a770f0
Stu Gott <http://issues.rpath.com/>
2007-05-10 17:23:25 -0400

    marshall new client call

--- mint/client.py
+++ mint/client.py
@@ -323,6 +323,16 @@ class MintClient:
         buildId = self.server.newBuild(projectId, buildName)
         return self.getBuild(buildId)
 
+    def newBuildsFromXml(self, projectId, buildXml):
+        """
+        Create a series of new builds from xml input.
+        @param projectId: the project to be associated with the new build.
+        @param buildXml: xml data describing the builds
+        @returns: a list of objects representing the new builds
+        """
+        buildIds = self.server.newBuildsFromXml(projectId, buildXml)
+        return [self.getBuild(x) for x in buildIds]
+
     def getBuildFilenames(self, buildId):
         """
         Returns a list of files and related data associated with a buildId
