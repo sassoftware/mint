f9872b56cf7d29117f15b7b63c79eedb4e9ee3b0
Jason Loomis <http://bugs.rpath.com>
2007-06-14 17:11:13 -0400

    add transport class for proxy

--- mint/web/admin.py
+++ mint/web/admin.py
@@ -7,6 +7,7 @@
 import os
 import sys
 import BaseHTTPServer
+import httplib
 import xmlrpclib
 import urllib
 import socket
@@ -29,6 +30,31 @@ from kid.pull import XML
 from conary import conarycfg, versions
 
 import simplejson
+
+class ProxiedTransport(xmlrpclib.Transport):
+    """
+    Transport class for contacting rUS through a proxy
+    """
+    def __init__(self, proxy):
+        splitUrl = urllib.splittype(proxy)
+        self.protocol = splitUrl[0]
+        self.proxy = splitUrl[1].lstrip('/')
+
+    def make_connection(self, host):
+        self.realHost = host
+        if self.protocol == 'https':
+            h = httplib.HTTPS(self.proxy)
+        else:
+            h = httplib.HTTP(self.proxy)
+        return h
+
+    def send_request(self, connection, handler, request_body):
+        connection.putrequest('POST', 'https://%s%s' % (self.realHost, 
+                                                         handler))
+
+    def send_host(self, connection, host):
+        connection.putheader('Host', self.realHost)
+
 
 class AdminHandler(WebHandler):
     def handle(self, context):
@@ -736,7 +762,16 @@ class AdminHandler(WebHandler):
                 user = '%s-%s-%s' % (group, servername, mirrorUrl)
             else:
                 user = '%s-%s' % (servername, mirrorUrl)
-            sp = xmlrpclib.ServerProxy("https://%s:%s@%s:8003/rAA/xmlrpc/" % (mirrorUser, mirrorPass, mirrorUrl))
+
+            # Deal with proxy
+            if 'https' in self.cfg.proxy.keys():
+                transport = ProxiedTransport(self.cfg.proxy['https'])
+            elif 'http' in self.cfg.proxy.keys():
+                transport = ProxiedTransport(self.cfg.proxy['http'])
+            else:
+                transport = None
+            
+            sp = xmlrpclib.ServerProxy("https://%s:%s@%s:8003/rAA/xmlrpc/" % (mirrorUser, mirrorPass, mirrorUrl), transport=transport)
             passwd = self._updateMirror(user, servername, mirrorUrl, sp)
 
         if not self._getErrors():
