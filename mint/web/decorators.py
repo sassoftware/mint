a337688f5ad59efc2f4ebb639bdacb039648b62e
Tim Gerla <http://issues.rpath.com/>
2007-07-18 14:27:19 -0400

    rename wrapper functions to distinct names for easier backtrace-reading

--- mint/web/decorators.py
+++ mint/web/decorators.py
@@ -14,26 +14,26 @@ from mint import userlevels
 from mint.web import webhandler
 
 def requiresHttps(func):
-    def wrapper(self, *args, **kwargs):
+    def requiresHttpsWrapper(self, *args, **kwargs):
         if self.req.subprocess_env.get('HTTPS', 'off') != 'on' and self.cfg.SSL:
             raise mint_error.PermissionDenied
         else:
             return func(self, *args, **kwargs)
 
-    return wrapper
+    return requiresHttpsWrapper
 
 def redirectHttp(func):
-    def wrapper(self, *args, **kwargs):
+    def redirectHttpWrapper(self, *args, **kwargs):
         if self.req.subprocess_env.get('HTTPS', 'off') != 'off' and self.cfg.SSL:
             return self._redirect('http://%s%s' % ( \
                 self.req.headers_in.get('host', self.req.hostname),
                 self.req.unparsed_uri))
         else:
             return func(self, *args, **kwargs)
-    return wrapper
+    return redirectHttpWrapper
 
 def redirectHttps(func):
-    def wrapper(self, *args, **kwargs):
+    def redirectHttpsWrapper(self, *args, **kwargs):
         reqPort = self.req.parsed_uri[apache.URI_PORT]
         reqHost = self.req.headers_in.get('host', self.req.hostname)
         if ':' not in reqHost and reqPort and reqPort != 443:
@@ -46,71 +46,71 @@ def redirectHttps(func):
                     (self.cfg.secureHost, self.req.unparsed_uri))
         else:
             return func(self, *args, **kwargs)
-    return wrapper
+    return redirectHttpsWrapper
 
 def requiresAdmin(func):
-    def wrapper(self, *args, **kwargs):
+    def requiresAdminWrapper(self, *args, **kwargs):
         if not kwargs['auth'].admin:
             raise mint_error.PermissionDenied
         else:
             return func(self, *args, **kwargs)
-    return wrapper
+    return requiresAdminWrapper
 
 def requiresAuth(func):
-    def wrapper(self, **kwargs):
+    def requiresAuthWrapper(self, **kwargs):
         if not kwargs['auth'].authorized:
             raise mint_error.PermissionDenied
         else:
             return func(self, **kwargs)
-    return wrapper
+    return requiresAuthWrapper
 
 def ownerOnly(func):
     """
     Require a method to be callable only by the owner of the current project.
     """
-    def wrapper(self, **kwargs):
+    def ownerOnlyWrapper(self, **kwargs):
         if not self.project:
             raise database.ItemNotFound("project")
         if self.userLevel == userlevels.OWNER or self.auth.admin:
             return func(self, **kwargs)
         else:
             raise mint_error.PermissionDenied
-    return wrapper
+    return ownerOnlyWrapper
 
 def writersOnly(func):
     """
     Require a method to be callable only by a developer or owner of a project.
     """
-    def wrapper(self, **kwargs):
+    def writersOnlyWrapper(self, **kwargs):
         if not self.project:
             raise database.ItemNotFound("project")
         if self.userLevel in userlevels.WRITERS or self.auth.admin:
             return func(self, **kwargs)
         else:
             raise mint_error.PermissionDenied
-    return wrapper
+    return writersOnlyWrapper
 
 def postOnly(func):
     """
     Require a method to be called with the POST HTTP method.
     """
-    def wrapper(self, *args, **kwargs):
+    def postOnlyWrapper(self, *args, **kwargs):
         if self.req.method != 'POST':
             raise webhandler.HttpForbidden
         else:
             return func(self, *args, **kwargs)
-    return wrapper
+    return postOnlyWrapper
 
 def mailList(func):
     """
     Decorate a method so that it is passed a MailingListClient object
     properly formatted and ready to use inside an error handler.
     """
-    def wrapper(self, **kwargs):
+    def mailListWrapper(self, **kwargs):
         mlists = mailinglists.MailingListClient(self.cfg.MailListBaseURL + 'RPC2')
         try:
             return func(self, mlists=mlists, **kwargs)
         except mailinglists.MailingListException, e:
             return self._write("error", shortError = "Mailing List Error",
                 error = "An error occured while talking to the mailing list server: %s" % str(e))
-    return wrapper
+    return mailListWrapper
