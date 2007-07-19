9dc835d18cc9e7c931b6da074322dd8558c0dd20
Scott Parkerson <http://issues.rpath.com/>
2007-07-19 16:25:15 -0400

    Check for outputUrl in serialization test

--- test/buildtest.py
+++ test/buildtest.py
@@ -776,7 +776,7 @@ class BuildTest(fixtures.FixturedUnitTest):
         self.failUnlessEqual(set(buildDict.keys()),
             set(['UUID', 'buildType', 'data', 'description', 'name', 'outputQueue',
              'project', 'serialVersion', 'troveFlavor', 'troveName',
-             'troveVersion', 'type', 'buildId']))
+             'troveVersion', 'type', 'buildId', 'outputUrl']))
 
         self.failUnlessEqual(set(buildDict['project']), set(['hostname', 'name', 'label', 'conaryCfg']))
 
