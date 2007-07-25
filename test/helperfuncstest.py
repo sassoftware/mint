3b73ca8121a345445f45600126c1a2f306b836e8
Stu Gott <http://issues.rpath.com/>
2007-07-25 14:51:58 -0400

    tweak makefiles adn tests

--- test/helperfuncstest.py
+++ test/helperfuncstest.py
@@ -92,7 +92,7 @@ class HelperFunctionsTest(unittest.TestCase):
         skipDirs = ('.hg', 'test/archive/arch', 'test/archive/use',
                     'mint/web/content', 'scripts', 'test/templates',
                     'test/annotate', 'test/coverage', 'test/.coverage',
-                    'test/archive/anaconda', 'bin', 'test')
+                    'test/archive/anaconda', 'bin', 'test', 'tom')
         mint_path = os.getenv('MINT_PATH')
 
         # tweak skipdirs to be fully qualified path
