261477173f2f3f6c7bfd2ca0e54dca6db29f5b0a
Scott Parkerson <http://issues.rpath.com/>
2007-10-17 09:16:33 -0400

    Have major and minor schema versions written to schema file

--- distro/schema.in.py
+++ distro/schema.in.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import sys
-sys.path.append('..')
+sys.path.insert(0,'..')
 
-from mint import schema
-print schema.RBUILDER_DB_VERSION
+from mint import schema, migrate
+print "%d.%d" % (migrate.majorMinor(schema.RBUILDER_DB_VERSION.major))
 
