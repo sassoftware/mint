f1bb673495226a20443a9a8b307cffcd243a51a8
Michael Tharp <https://issues.rpath.com/>
2007-12-05 12:34:06 -0500

    Some raa test fixes. (RBL-2445)

--- test/raa-tests/runtests.sh
+++ test/raa-tests/runtests.sh
@@ -6,4 +6,40 @@
 
 rm -f .coverage
 
-PYTHONPATH=../../../conary-test-1.1:../../:../../raaplugins/:../../../conary/:../../../raa-2.1/:../../../raa-test-2.1/:$PYTHONPATH nosetests -v -p rPath $@ 2>&1
+if [ -z "$CONARY_PATH" ]
+then
+    CONARY_PATH=../../../conary
+fi
+
+if [ -z "$CONARY_TEST_PATH" ]
+then
+    CONARY_TEST_PATH=../../../conary-test
+fi
+
+if [ -z "$RAA_PATH" ]
+then
+    RAA_PATH=../../../raa-2.1
+fi
+
+if [ -z "$RAA_TEST_PATH" ]
+then
+    RAA_TEST_PATH=../../../raa-test-2.1
+fi
+
+if [ -z "$MINT_PATH" ]
+then
+    MINT_PATH=../..
+fi
+
+if [ -z "$PLUGINS_PATH" ]
+then
+    PLUGINS_PATH=../../raaplugins
+fi
+
+if [ -z "$COVERAGE_PATH" ]
+then
+    COVERAGE_PATH=../../../utils
+fi
+
+export CONARY_PATH CONARY_TEST_PATH RAA_PATH RAA_TEST_PATH MINT_PATH COVERAGE_PATH
+PYTHONPATH="$CONARY_PATH:$CONARY_TEST_PATH:$RAA_PATH:$RAA_TEST_PATH:$MINT_PATH:$PLUGINS_PATH:$COVERAGE_PATH:$PYTHONPATH" nosetests -v -p rPath "$@"
