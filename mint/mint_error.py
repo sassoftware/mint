21e66abee070af01722aec472c612d3313ce2d24
Brad Murphy <https://issues.rpath.com>
2008-07-02 17:44:47 -0400

    Fixed error typo

--- mint/mint_error.py
+++ mint/mint_error.py
@@ -210,7 +210,7 @@ class ProductDefinitionError(MintError):
     def freeze(self): return (self.reason,)
 
     def __str__(self):
-        return "There is was a problem that occurred when tryin to access the product definition for %s" % self.reason
+        return "There is was a problem that occurred when trying to access the product definition for %s" % self.reason
 
 class UnmountFailed(MintError):
     def __init__(self, dev):
