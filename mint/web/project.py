c3688daa3333336fe1977266dd485f5a11f1e556
Scott Parkerson <http://issues.rpath.com/>
2008-08-14 09:53:00 -0400

    Fix traceback where trovepicker has put "NONE" in the field (RBL-3259)

--- mint/web/project.py
+++ mint/web/project.py
@@ -688,6 +688,7 @@ class ProjectHandler(BaseProjectHandler, PackageCreatorMixin):
                 anacondaVars[key] = build.getDataValue(key, validate = False)
                 if anacondaVars[key]:
                     n,v,f = parseTroveSpec(anacondaVars[key])
+                    if not v: continue  # skip things that don't have valid versions
                     try:
                         vObj = versions.VersionFromString(v)
                     except ParseError:
