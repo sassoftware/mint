60e28840b1d825c2a79682da792cde293f888497
David Christian <http://issues.rpath.com>
2008-09-10 18:02:09 -0400

    set archiveDir

--- test/fixtures.py
+++ test/fixtures.py
@@ -881,6 +881,11 @@ class FixturedUnitTest(testhelp.TestCase, MCPTestMixin):
             os.dup2(oldOut, sys.stdout.fileno())
 
     def setUp(self):
+        for dir in sys.path:
+            thisdir = os.path.normpath(os.sep.join((dir, 'archive')))
+            if os.path.isdir(thisdir):
+                self.archiveDir = thisdir
+                break
         testhelp.TestCase.setUp(self)
         MCPTestMixin.setUp(self)
 
