38532df0ae2958e56b95241d5531b8b06882fd30
Michael Tharp <mtharp@rpath.com>
2009-01-12 23:20:53 +0000

    Fix transparency on anaconda splash.

--- distro/artwork/regen.sh
+++ distro/artwork/regen.sh
@@ -9,12 +9,22 @@ fi
 
 rm -f *.tmp.png
 
-# Straight PNG conversion
-for base in anaconda_splash bootloader_splash
+# PNGs without transparency (strips out alpha channel)
+for base in bootloader_splash
 do
     echo Generating "$base"
     inkscape --without-gui --file "${base}.svg" --export-png "${base}.tmp.png" --export-area-canvas || exit 1
     pngcrush -q -c 2 "${base}.tmp.png" "${base}.png" || exit 1
+    rm -f "${base}.tmp.png" "${base}.tmp2.png"
+    ls -lh "${base}.png"
+done
+
+# PNGs with transparency
+for base in anaconda_splash
+do
+    echo Generating "$base"
+    inkscape --without-gui --file "${base}.svg" --export-png "${base}.tmp.png" --export-area-canvas || exit 1
+    pngcrush -q -c 6 "${base}.tmp.png" "${base}.png" || exit 1
     rm -f "${base}.tmp.png" "${base}.tmp2.png"
     ls -lh "${base}.png"
 done
--- mint/web/content/images/regen.sh
+++ mint/web/content/images/regen.sh
@@ -9,14 +9,22 @@ fi
 
 rm -f *.tmp.png
 
-# Straight PNG conversion
-# NB: pngcrush is configured to strip out transparency;
-# make a new section for files that need it.
+# PNGs without transparency (strips out alpha channel)
 for base in prodlogo
 do
     echo Generating "$base"
     inkscape --without-gui --file "${base}.svg" --export-png "${base}.tmp.png" --export-area-canvas || exit 1
     pngcrush -q -c 2 "${base}.tmp.png" "${base}.png" || exit 1
+    rm -f "${base}.tmp.png" "${base}.tmp2.png"
+    ls -lh "${base}.png"
+done
+
+# PNGs with transparency
+for base in 
+do
+    echo Generating "$base"
+    inkscape --without-gui --file "${base}.svg" --export-png "${base}.tmp.png" --export-area-canvas || exit 1
+    pngcrush -q -c 6 "${base}.tmp.png" "${base}.png" || exit 1
     rm -f "${base}.tmp.png" "${base}.tmp2.png"
     ls -lh "${base}.png"
 done
