0477530530224110a2493a76e99054ed2002949e
David Christian <http://issues.rpath.com>
2009-02-13 11:41:49 -0500

    schema moved

--- distro/schema.in.py
+++ distro/schema.in.py
@@ -2,7 +2,7 @@
 import sys
 sys.path.insert(0,'..')
 
-from mint import schema
+from mint.db import schema
 from mint.scripts import migrate
 print "%d.%d" % (migrate.majorMinor(schema.RBUILDER_DB_VERSION.major))
 
