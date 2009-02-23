57ec651e10d2b0cf538d0dd3ff7b5d11bed9aab6
David Christian <http://issues.rpath.com>
2009-02-23 11:28:55 -0500

    add missing file

---
+++ mint/rest/modellib/options.py
@@ -0,0 +1,6 @@
+
+class Options(object):
+    def __init__(self, cls, meta):
+        className = cls.__name__
+        name = className[0].lower()  + className[1:]
+        self.name = getattr(meta, 'name', name)
