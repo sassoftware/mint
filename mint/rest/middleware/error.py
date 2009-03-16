279d5f9545d8becaa212a0f282dd3ebf5da7dcf0
David Christian <http://issues.rpath.com>
2009-03-16 12:09:21 -0400

    Add xml/json error handling code.

--- mint/rest/middleware/error.py
+++ mint/rest/middleware/error.py
@@ -1,7 +1,37 @@
+#
+# Copyright (c) 2009 rPath, Inc.
+#
+# All Rights Reserved
+#
+import traceback
 
-class ErrorResponse(object):
+from restlib import response
+
+from mint.rest.api import models
+from mint.rest.modellib import converter
+
+class ErrorCallback(object):
+    def __init__(self, controller):
+        self.controller = controller
+        
     def processException(self, request, excClass, exception, tb):
-        content = ['%s: %s\nTraceback:\n' % (excClass.__name__, exception)]
-        content.append(''.join(traceback.format_tb(tb)))
-        return Response(status=500, content=''.join(content), content_type='text/plain')
-
+        message = '%s: %s' % (excClass.__name__, exception)
+        tbString = 'Traceback:\n' + ''.join(traceback.format_tb(tb))
+        if hasattr(exception, 'status'):
+            status = exception.status
+        else:
+            status = 500
+        if request.contentType == 'text/plain':
+            # just print out the traceback in the easiest to read
+            # format.
+            text = [message, tbString]
+        else:
+            code = status
+            if 'HTTP_X_FLASH_VERSION' in request.headers:
+                status = 200
+            error = models.Error(code=code, message=message, 
+                                 traceback=tbString)
+            text = converter.toText(request.responseType, error,
+                                    self.controller, request)
+        return response.Response(text, content_type=request.contentType,
+                                 status=status)
