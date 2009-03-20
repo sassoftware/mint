0e07ece33d7423a4276c02400ba8b9e378f72c88
David Christian <http://issues.rpath.com>
2009-03-20 14:53:16 -0400

    base imagePath on self.mintCfg.dataPath

--- mint_test/resttest/apitest/repostest.py
+++ mint_test/resttest/apitest/repostest.py
@@ -208,7 +208,7 @@ Index.
         <title>Image File 1</title>
         <size>1024</size>
         <sha1>356a192b7913b04c54574d18c28d46e6395428ab</sha1>
-        <url urlType="0">/tmp/rbuildertest-misa/repos-mint/images/testproject/2/imagefile_2.iso</url>
+        <url urlType="0">%(data)s/images/testproject/2/imagefile_2.iso</url>
       </file>
     </files>
   </image>
@@ -237,7 +237,7 @@ Index.
         <title>Image File 2</title>
         <size>2048</size>
         <sha1>da4b9237bacccdf19c0760cab7aec4a8359010b0</sha1>
-        <url urlType="0">/tmp/rbuildertest-misa/repos-mint/images/testproject/2/imagefile_2.iso</url>
+        <url urlType="0">%(data)s/images/testproject/2/imagefile_2.iso</url>
       </file>
     </files>
   </image>
@@ -249,7 +249,8 @@ Index.
                       resp)
 
         self.failUnlessEqual(resp,
-             exp % dict(port = client.port, server = client.server))
+             exp % dict(port = client.port, server = client.server,
+                        data = self.mintCfg.dataPath))
 
 
 if __name__ == "__main__":
