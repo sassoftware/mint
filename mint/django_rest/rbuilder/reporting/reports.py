6f272c19cda220930f0b2ac7a8acd4e3f6b5227b
Ben Rector <https://issues.rpath.com/>
2009-11-06 10:10:41 -0500

    zero fill segments for reports

--- mint/django_rest/rbuilder/reporting/reports.py
+++ mint/django_rest/rbuilder/reporting/reports.py
@@ -2,6 +2,8 @@ from mint.django_rest import rbuilder
 
 from xobj import xobj
 
+import time
+
 class TimeSegmentReport(object):
 
     _xobj = xobj.XObjMetadata(
@@ -42,6 +44,47 @@ class TimeSegmentReport(object):
             self.startTime = segment.time
         if segment.time > self.endTime:   
             self.endTime = segment.time
+            
+        if self.timeSegments.timeSegment:
+            self._fillInBlanks(self.timeSegments.timeSegment[-1], segment)    
         self.timeSegments.timeSegment.append(segment)
+        
+    def _fillInBlanks(self, segment, nextSegment):
+        timeindex = self._incrementTime(segment.time, self.timeUnits)
+        while timeindex < nextSegment.time:	        
+	        zeroSeg = self.TimeSegment.Segment(0, timeindex)
+	        self.timeSegments.timeSegment.append(zeroSeg)
+	        timeindex =  self._incrementTime(timeindex, self.timeUnits)
+    
+    def _incrementTime(self, time, units):
+        return self._time_units_matrix[units]['delta'](self,time)
+    
+    def _incrementYear(self, starttime):
+        timetuple = time.localtime(starttime)
+        timelist = list(timetuple)
+        timelist[0] = timelist[0] + 1
+        return time.mktime(tuple(timelist))
+        
+    def _incrementMonth(self, starttime):
+        timetuple = time.localtime(starttime)
+        timelist = list(timetuple)
+        if timelist[1] == 12:
+            timelist[0] = timelist[0] + 1
+            timelist[1] = 1
+        else:
+            timelist[1] = timelist[1] + 1 
+        return time.mktime(tuple(timelist))           
+        
+    def _incrementDay(self, starttime):
+        return starttime + 60 * 60 * 24
+        
+    def _incrementHour(self, starttime):
+        return starttime + 60 * 60
 
+    # Matrix of time units to the order of time elements to have distinct values
+    _time_units_matrix = {'year': {'delta' : _incrementYear},
+                           'month': {'delta' : _incrementMonth},
+                           'day': {'delta' : _incrementDay},
+                           'hour': {'delta' : _incrementHour},     
+                          }
         
