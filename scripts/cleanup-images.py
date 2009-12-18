f93ca173f166535e25c2db9e13de4eaf3d688656
Michael Tharp <mtharp@rpath.com>
2009-12-18 11:39:32 -0500

    Update image cleanup script to handle orphaned filesurls

--- scripts/cleanup-images.py
+++ scripts/cleanup-images.py
@@ -1,62 +1,85 @@
 #!/usr/bin/python
-
-import os, sys
-import pwd
-
-dryRun = False
-if len(sys.argv) > 1 and sys.argv[1] == "--dry-run":
-    dryRun = True
-elif len(sys.argv) > 1:
-    print "usage: cleanup-images.py [--dry-run]"
-    sys.exit(1)
-
-if not dryRun and os.geteuid():
-    print >> sys.stderr, "Script not run as root: forcing --dry-run"
-    dryRun = True
+import optparse
+import os
+import sys
 
 from conary import dbstore
 from mint import config
 from mint import urltypes
 
-apacheGid = pwd.getpwnam('apache')[3]
-isogenUid = pwd.getpwnam('isogen')[2]
 
-cfg = config.MintConfig()
-cfg.read(config.RBUILDER_CONFIG)
-db = dbstore.connect(cfg.dbPath, cfg.dbDriver)
-cu = db.cursor()
-cu.execute('SELECT url FROM FilesUrls WHERE urlType = ?', urltypes.LOCAL)
-imageFiles = [x[0] for x in cu.fetchall()]
+def main(args):
+    parser = optparse.OptionParser()
+    parser.add_option('-c', '--config-file', default=config.RBUILDER_CONFIG)
+    parser.add_option('-n', '--dry-run', action='store_true')
+    options, args = parser.parse_args(args)
+    if args:
+        parser.error("No arguments allowed")
 
-deleteCount = bytesDeletedCount = long(0)
-for baseDir, dirs, files in os.walk(cfg.imagesPath):
-    if len(baseDir.split(os.path.sep)) == 6:
-        if dryRun:
-            print "# ++ UPDATING PERMISSIONS/OWNERSHIP ON %s" % baseDir
-            print "chown %d.%d %s" % (isogenUid, apacheGid, baseDir)
-            print "chmod %0o %s" % (os.stat(baseDir)[0] & 0777 | 0020, baseDir)
-        else:
-            os.chown(baseDir, isogenUid, apacheGid)
-            os.chmod(baseDir, os.stat(baseDir)[0] & 0777 | 0020)
-        for file in files:
-            path = os.path.join(baseDir, file)
-            if path not in imageFiles:
-                if dryRun:
-                    print "# -- DELETING IMAGE %s" % path
-                    print "rm -f %s" % path
-                else:
-                    os.unlink(path)
-                deleteCount += 1
-                bytesDeletedCount += os.stat(path)[6]
-            else:
-                if dryRun:
-                    print "chown %d.%d %s" % (isogenUid, apacheGid, path)
-                else:
-                    os.chown(path, isogenUid, apacheGid)
+    cfg = config.MintConfig()
+    cfg.read(options.config_file)
 
-sys.stdout.flush()
-if dryRun:
-    print >> sys.stderr, "# -- %d images would have been deleted (%ld MBytes)" % (deleteCount, (bytesDeletedCount / (1024 * 1024)))
-else:
-    print >> sys.stderr, "%d images deleted (%ld MBytes)" % (deleteCount, (bytesDeletedCount / (1024 * 1024)))
-sys.stderr.flush()
+    db = dbstore.connect(cfg.dbPath, cfg.dbDriver)
+    cu= db.cursor()
+
+    print >> sys.stderr, "Cleaning up orphaned FilesUrls entries ..."
+    db.transaction()
+    cu.execute("""SELECT fileid, urlid, url FROM buildfilesurlsmap
+            JOIN filesurls USING ( urlid ) WHERE urltype = ?""", urltypes.LOCAL)
+    toDelete = []
+    for fileId, urlId, path in cu:
+        if not os.path.exists(path):
+            toDelete.append(urlId)
+    print >> sys.stderr, "Deleting %d dead FilesUrls entries" % len(toDelete)
+    if toDelete:
+        cu.execute("DELETE FROM filesurls WHERE urlid IN ( %s )" % (
+                ', '.join('%d' % x for x in toDelete)))
+    cu.execute("""DELETE FROM buildfiles a WHERE NOT EXISTS (
+            SELECT * FROM buildfilesurlsmap b WHERE a.fileid = b.fileid )""")
+    if options.dry_run:
+        db.rollback()
+    else:
+        db.commit()
+
+    print >> sys.stderr, "Cleaning up orphaned image files ..."
+    trashedFiles = trashedDirs = trashedSize = 0
+    for shortname in os.listdir(cfg.imagesPath):
+        # list files on disk
+        projpath = os.path.join(cfg.imagesPath, shortname)
+        actualFiles = set()
+        for dirpath, dirnames, filenames in os.walk(projpath):
+            actualFiles.update(os.path.join(dirpath, x) for x in filenames)
+
+        # list files in db
+        cu.execute("""SELECT url FROM projects
+                JOIN builds USING ( projectid )
+                JOIN buildfiles USING ( buildid )
+                JOIN buildfilesurlsmap USING ( fileid )
+                JOIN filesurls USING ( urlid )
+                WHERE shortname = ? AND urltype = ?
+                """, shortname, urltypes.LOCAL)
+        expectedFiles = set(x[0] for x in cu)
+
+        # delete unmanaged files
+        for path in actualFiles - expectedFiles:
+            print >> sys.stderr, "Deleting", path
+            trashedFiles += 1
+            trashedSize += os.lstat(path).st_size
+            if not options.dry_run:
+                os.unlink(path)
+
+        # clean up empty dirs
+        if not options.dry_run:
+            for dirpath, dirnames, filenames in os.walk(projpath,
+                    topdown=False):
+                if not dirnames and not filenames:
+                    os.rmdir(dirpath)
+                    trashedDirs += 1
+
+    print >> sys.stderr, ("Deleted %d files and %d directories "
+            "containing %d MiB" % (trashedFiles, trashedDirs,
+                    trashedSize / 1048576.0))
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
