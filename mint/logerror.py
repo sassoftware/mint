07f8180d3e6aca3729f9f8189fc2467011e987fb
Michael Tharp <mtharp@rpath.com>
2010-01-04 09:58:46 -0500

    Un-disable traceback emails.

--- mint/logerror.py
+++ mint/logerror.py
@@ -112,7 +112,6 @@ def logErrorAndEmail(cfg, e_type, e_value, e_tb, location, info_dict,
 
     # send email
     if cfg and doEmail:
-        return
         base_exception = traceback.format_exception_only(
             e_type, e_value)[-1].strip()
         if cfg.rBuilderOnline:
