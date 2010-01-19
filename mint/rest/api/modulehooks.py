dbfad2df5fe2da880f59296959f3306a037b752f
Ben Rector <https://issues.rpath.com/>
2010-01-19 09:41:03 -0500

    fixed modulehooks api to be public visable

--- mint/rest/api/modulehooks.py
+++ mint/rest/api/modulehooks.py
@@ -11,9 +11,12 @@ from restlib import response
 from mint.rest.api import base
 from mint.rest.api import models
 
+from mint.rest.middleware import auth
+
 class ModuleController(base.BaseController):
     modelName = 'modules'
 
+    @auth.public
     def index(self, request):
         moduleHooksList = models.ModuleHooks()
         for path, dirs, files in os.walk(self.cfg.moduleHooksDir):
