7be731ffa34fa5c44111c04d883d596ef6c9e76a
James Slagle <slagle@rpath.com>
2010-04-06 18:16:05 -0400

    RBL-6025 mark generated models as not managed by django

--- mint/django_rest/rbuilder/inventory/models.py
+++ mint/django_rest/rbuilder/inventory/models.py
@@ -9,7 +9,7 @@ from django.db import models
 from mint.django_rest.rbuilder import models as rbuildermodels
 from mint.django_rest.rbuilder.inventory.xml_1_0 import system
 
-class ManagedSystems(models.Model):
+class ManagedSystem(models.Model):
     parser = system.managedSystemTypeSub
     registrationDate = models.DateTimeField('Registration Date')
     generatedUUID = models.CharField(max_length=64, null=True)
@@ -18,20 +18,20 @@ class ManagedSystems(models.Model):
     sslClientKey = models.CharField(max_length=8092, null=True)
     sslServerCertificate = models.CharField(max_length=8092, null=True)
 
-class SystemsTargets(models.Model):
-    managedSystemId = models.ForeignKey(ManagedSystems)
-    targetId = models.ForeignKey(rbuildermodels.Targets)
+class SystemTarget(models.Model):
+    managedSystem = models.ForeignKey(ManagedSystem, null=True)
+    target = models.ForeignKey(rbuildermodels.Targets)
     targetSystemId = models.CharField(max_length=256, null=True)
 
-class SoftwareVersions(models.Model):
+class SoftwareVersion(models.Model):
     softwareVersion = models.TextField(unique=True)
 
-class SystemsSoftwareVersions(models.Model):
-    managedSystemId = models.ForeignKey(ManagedSystems)
-    softwareVersionId = models.ForeignKey(SoftwareVersions)
+class SystemSoftwareVersion(models.Model):
+    managedSystem = models.ForeignKey(ManagedSystem)
+    softwareVersion = models.ForeignKey(SoftwareVersion)
 
 class SystemInformation(models.Model):
-    managedSystemId = models.ForeignKey(ManagedSystems)
+    managedSystem = models.ForeignKey(ManagedSystem)
     systemName = models.CharField(max_length=64, null=True)
     memory = models.IntegerField(null=True)
     osType = models.CharField(max_length=64, null=True)
@@ -40,20 +40,20 @@ class SystemInformation(models.Model):
     systemType = models.CharField(max_length=32, null=True)
 
 class NetworkInformation(models.Model):
-    managedSystemId = models.ForeignKey(ManagedSystems)
+    managedSystem = models.ForeignKey(ManagedSystem)
     interfaceName = models.CharField(max_length=32, null=True)
     ipAddress = models.CharField(max_length=15, null=True)
     netmask = models.CharField(max_length=20, null=True)
     portType = models.CharField(max_length=32, null=True)
 
-class StorageVolumes(models.Model):
-    managedSystemId = models.ForeignKey(ManagedSystems)
+class StorageVolume(models.Model):
+    managedSystem = models.ForeignKey(ManagedSystem)
     size = models.IntegerField(null=True)
     storageType = models.CharField(max_length=32, null=True)
     storageName = models.CharField(max_length=32, null=True)
 
-class CPUs(models.Model):
-    managedSystemId = models.ForeignKey(ManagedSystems)
+class CPU(models.Model):
+    managedSystem = models.ForeignKey(ManagedSystem)
     cpuType = models.CharField(max_length=64, null=True)
     cpuCount = models.IntegerField(null=True)
     cores = models.IntegerField(null=True)
