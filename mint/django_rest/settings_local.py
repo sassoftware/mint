165fc5d1ff9ad9ad81e00eded43ba183b639f11f
James Slagle <slagle@rpath.com>
2010-05-21 11:27:48 -0400

    More changes for a local development env

--- mint/django_rest/settings_local.py
+++ mint/django_rest/settings_local.py
@@ -3,7 +3,13 @@
 # First, import everything from the production settings.py
 from settings import *
 
+ROOT_URLCONF = 'mint.django_rest.urls-local'
+
 # Override individual options
 DEBUG = False
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'mint-local.db'             # Or path to database file if using sqlite3.
+
+appsList = list(INSTALLED_APPS)
+appsList.append('django.contrib.admin')
+INSTALLED_APPS = tuple(appsList)
