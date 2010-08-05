119748c35126009f4198bd7c88ecf217694e3272
James Slagle <slagle@rpath.com>
2010-08-05 15:01:00 -0400

    Fix python 2.4 incompatiblity

--- mint/django_rest/settings_local.py
+++ mint/django_rest/settings_local.py
@@ -6,7 +6,7 @@ from mint.django_rest.settings_common import *  # pyflakes=ignore
 ROOT_URLCONF = 'mint.django_rest.urls_local'
 
 # Override individual options
-DEBUG = True
+DEBUG = False
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'mint-local.db'             # Or path to database file if using sqlite3.
 
@@ -18,6 +18,14 @@ AUTHENTICATION_BACKENDS = (
     'mint.django_rest.rbuilder.auth.rBuilderBackend',
     'django.contrib.auth.backends.ModelBackend'
 )
+
+for i in range(len(MIDDLEWARE_CLASSES)):
+    if MIDDLEWARE_CLASSES[i] == \
+        'mint.django_rest.middleware.AddCommentsMiddleware':
+        break
+
+MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES[0:i] + \
+                     MIDDLEWARE_CLASSES[i+1:]
 
 # Custom setting for if we should manage/create the tables in rbuilder.models
 MANAGE_RBUILDER_MODELS = True
