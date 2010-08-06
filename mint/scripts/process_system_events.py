29bd5a08ad5a9ab114b7dfe9ec71309e864f733a
Brad Murphy <https://issues.rpath.com>
2010-08-06 09:58:12 -0400

    more system event processing log work

--- mint/scripts/process_system_events.py
+++ mint/scripts/process_system_events.py
@@ -24,12 +24,12 @@ class ProcessSystemEvents(scriptlibrary.SingletonScript):
             self.cfgPath = sys.argv.pop(0).split('=')[1]
             print "Test mode using configuration from %s" % self.cfgPath
             
-        quitMode = False
+        quietMode = False
         if "-q" in sys.argv:
-            quitMode = True
+            quietMode = True
             
         self.loadConfig(cfgPath=self.cfgPath)
-        self.resetLogging(quiet=quitMode)
+        self.resetLogging(quiet=quietMode)
     
         settingsModule = "mint.django_rest.settings"
         if len(sys.argv) > 1 and sys.argv[1] == 'useLocalSettings':
