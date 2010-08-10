648679b34f8a0436b12cbc6f153c7e995a264383
Brad Murphy <https://issues.rpath.com>
2010-08-10 11:25:50 -0400

    initial creation of target system import script with hourly cron task and logrotate setup - now with files

---
+++ mint/scripts/target_systems_import.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2010 rPath, Inc.
+#
+# All Rights Reserved
+#
+"""
+This script can be used to import running systems from predefined targets
+"""
+
+import os
+import sys
+import logging
+
+from mint import config
+from mint.lib import scriptlibrary
+
+class TargetSystemsImport(scriptlibrary.SingletonScript):
+    cfgPath = config.RBUILDER_CONFIG
+    logFileName = 'target_systems_import.log'
+    newLogger = True
+    
+    def run(self):        
+        if sys.argv[0].startswith('--xyzzy='):
+            self.cfgPath = sys.argv.pop(0).split('=')[1]
+            print "Test mode using configuration from %s" % self.cfgPath
+            
+        quietMode = False
+        if "-q" in sys.argv:
+            quietMode = True
+            
+        self.loadConfig(cfgPath=self.cfgPath)
+        self.resetLogging(quiet=quietMode, fileLevel=logging.INFO)
+    
+        settingsModule = "mint.django_rest.settings"
+        if len(sys.argv) > 1 and sys.argv[1] == 'useLocalSettings':
+            settingsModule = "mint.django_rest.settings_local"
+            os.environ['MINT_LOCAL_DB'] = os.path.realpath("../mint/mint-local.db")
+            
+        os.environ['DJANGO_SETTINGS_MODULE'] = settingsModule
+        
+        from mint.django_rest.rbuilder.inventory import systemdbmgr
+        system_manager = systemdbmgr.SystemDBManager()
+        system_manager.importTargetSystems()
+        
+    def usage(self):
+        print >> sys.stderr, "Usage: %s [useLocalSettings]" % \
+            sys.argv[0]
+        sys.stderr.flush()
+        sys.exit(1)
