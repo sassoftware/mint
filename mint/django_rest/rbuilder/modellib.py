d3ff3957890b5ba9c9cacf073a4874fbeaac4cce
James Slagle <slagle@rpath.com>
2010-08-13 12:23:03 -0400

    Refactor object deserialization code to be more readable, add comments

--- mint/django_rest/rbuilder/modellib.py
+++ mint/django_rest/rbuilder/modellib.py
@@ -14,17 +14,44 @@ from xobj import xobj
 type_map = {}
 
 class BaseManager(models.Manager):
-    _transient = True
 
-    def load(self, model_inst):
+    def load_from_db(self, model_inst):
+        """
+        Load a model from the db based on model_inst.  Uses load_fields on the
+        model to look up a corrosponding model in the db. 
+
+        Override this method for more specific behavior for a given model.
+        """
         try:
             loaded_model = self.get(**model_inst.load_fields_dict())
+            return loaded_model
         except exceptions.ObjectDoesNotExist:
             return None
         except exceptions.MultipleObjectsReturned:
             return None
+
+
+    def load(self, model_inst):
+        """
+        Load a model based on model_inst, which is an instance of the model.
+        Allows for checking to see if model_inst already exists in the db, and
+        if it does returns a corrosponding model with the correct fields set
+        (such as pk, which wouldn't be set on model_inst).
+
+        If a matching model was found, update it's fields with the values from
+        model_inst.
+        """
+
+        loaded_model = self.load_from_db(model_inst)
+
+        # For each field on loaded_model, see if that field is defined on
+        # model_inst, if it is and the value is different, update the value on
+        # loaded_model.  In this case we are most likely handling a PUT or an
+        # update to a model.
         if loaded_model:
             for field in loaded_model._meta.fields:
+                # django throws ObjectDoesNotExist if you try to access a
+                # related model that doesn't exist, so just swallow it.
                 try:
                     if getattr(model_inst, field.name) is None:
                         continue
@@ -35,46 +62,88 @@ class BaseManager(models.Manager):
                     setattr(loaded_model, field.name, 
                         getattr(model_inst, field.name))
             return loaded_model
-        else:
-            return None
 
-    def load_from_href(self, obj, request):
-        href = getattr(obj, 'href', None)
+        return loaded_model
+
+    def load_or_create(self, model_inst):
+        """
+        Similar in vein to django's get_or_create API.  Try to load a model
+        from the db, if one wasn't found, create one and return it.
+        """
+        created = False
+        # Load the model from the db.
+        loaded_model = self.load(model_inst)
+        if not loaded_model:
+            # No matching model was found. We need to save.  This scenario
+            # means we must be creating something new (POST), so it's safe to
+            # go ahead and save here, if something goes wrong later, this will
+            # be automatically rolled back.
+            model_inst.save()
+            created = True
+            loaded_model = model_inst
+
+        return created, loaded_model
+
+    def load_from_href(self, href):
+        """
+        Given an href, return the corrosponding model.
+        """
         if href:
             path = urlparse.urlparse(href)[2]
+            # Look up the view function that corrosponds to the href using the
+            # django API.
             resolver = urlresolvers.resolve(path)
+            # resolver contains a function and it's arguments that django
+            # would use to call the view.
             func, args = resolver[0:2]
-            model = func.get(*args)
-            return model
+            # The django rest api always uses .get(...) to handle a GET, which
+            # will always return a model.
+            return func.get(*args)
         else:
             return None
 
-    def load_from_object(self, obj, request, save=True):
-        model = self.model()
+    def add_fields(self, model, obj):
+        """
+        For each obj attribute, if the attribute matches a field name on
+        model, set the attribute's value on model.
+        """
+        # dict of field names and field instances (these are not field values)
         fields = {}
         for f in model._meta.fields:
             fields[f.name] = f
-        accessors = {}
-        for r in model._meta.get_all_related_objects():
-            accessors[r.get_accessor_name()] = r
+
         for key, val in obj.__dict__.items():
             if key in fields.keys():
+                # Special case for FK fields which should be hrefs.
                 if isinstance(fields[key], related.RelatedField):
-                    val = fields[key].related.parent_model.objects.load_from_href(val, request)
+                    val = fields[key].related.parent_model.objects.load_from_href(
+                        getattr(val, 'href', None))
                 elif val:
                     if fields[key].primary_key:
                         val = int(val)
                     else:
+                        # Cast to str, django will just do the right thing.
                         val = str(val)
                 else:
                     val = None
+
                 setattr(model, key, val)
 
-        loaded_model = self.load(model)
-        if not loaded_model:
-            if save:
-                model.save()
-            loaded_model = model
+        return model
+
+    def add_accessors(self, model, obj, request=None):
+        """
+        For each obj attribute, if the attribute matches an accessor name,
+        load all the acccessor models off obj and add them to model.
+        """
+        # dict of accessor names and instances.  an accessor in django are the
+        # reverse foreign key relationships, e.g., network has a FK to system,
+        # so system models have a system.networks attribute which refers to
+        # all network models that reference that system.
+        accessors = {}
+        for r in model._meta.get_all_related_objects():
+            accessors[r.get_accessor_name()] = r
+
         for key, val in obj.__dict__.items():
             if key in accessors.keys():
                 rel_obj_name = accessors[key].var_name
@@ -84,10 +153,34 @@ class BaseManager(models.Manager):
                 if type(rel_objs) != type([]):
                     rel_objs = [rel_objs]
                 for rel_obj in rel_objs:
+                    # We're creating related models, such as a network model
+                    # that's going to reference a system, well since system is
+                    # not yet set as  FK, we can't save the network model yet,
+                    # so pass False for the save flag.
                     rel_mod = type_map[rel_obj_name].objects.load_from_object(
                         rel_obj, request, save=False)
-                    getattr(loaded_model, key).add(rel_mod)
-        return loaded_model
+                    getattr(model, key).add(rel_mod)
+        return model
+
+    def load_from_object(self, obj, request, save=True):
+        """
+        Given an object (obj) from xobj, create and return the  corrosponding
+        model, loading it from the db if one exists.  obj does not have to be
+        from xobj, but it should match the similar structure of an object that
+        xobj would create from xml.
+        """
+        # Every manager has access to the model it's a manager for, create an
+        # emtpy model instance to start with.
+        model = self.model()
+
+        model = self.add_fields(model, obj)
+        if save:
+            created, model = self.load_or_create(model)
+
+        model = self.add_accessors(model, obj, request)
+
+        return model
+
     
 class XObjModel(models.Model):
     objects = BaseManager()
