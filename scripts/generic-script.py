bc8ecce3699b22ca0fd80c67172a39d6afded567
Michael Tharp <mtharp@rpath.com>
2010-08-23 10:09:51 -0400

    Fix bug in generic script when running from checkout

--- scripts/generic-script.py
+++ scripts/generic-script.py
@@ -4,7 +4,7 @@ import sys
 
 maybeRoot = os.path.dirname(os.path.dirname(__file__))
 if os.path.exists(os.path.join(maybeRoot, 'mint', 'lib')):
-    sys.path.insert(maybeRoot)
+    sys.path.insert(0, maybeRoot)
 
 from mint.scripts import some_module
 sys.exit(some_module.Script().run())
