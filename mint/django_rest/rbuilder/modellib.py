490c1a46241cb82a5a1e477ac041557acd81e254
Mihai Ibanescu <https://issues.rpath.com/>
2010-09-01 12:29:35 -0400

    Save last job that got updated so we can change the system state (RBL-6971)

--- mint/django_rest/rbuilder/modellib.py
+++ mint/django_rest/rbuilder/modellib.py
@@ -146,7 +146,7 @@ class BaseManager(models.Manager):
             if isinstance(field, SerializedForeignKey):
                 val = field.related.parent_model.objects.load_from_object(val, request, save=save)
             elif isinstance(field, InlinedForeignKey):
-                lookup = { field.visible : val }
+                lookup = { field.visible : str(val) }
                 # Look up the inlined value
                 val = field.related.parent_model.objects.get(**lookup)
             elif isinstance(field, related.RelatedField):
@@ -308,8 +308,14 @@ class BaseManager(models.Manager):
         model = self.add_m2m_accessors(model, obj, request)
         model = self.add_list_fields(model, obj, request, save=save)
         model = self.add_accessors(model, accessors)
+        self.postprocess(model)
 
         return model
+
+    def postprocess(self, model):
+        method = getattr(model, 'postprocess', None)
+        if method is not None:
+            method()
 
 class TroveManager(BaseManager):
     def load_from_object(self, obj, request, save=True):
@@ -405,6 +411,7 @@ class SystemManager(BaseManager):
             BaseManager.set_m2m_accessor(self, model, m2m_accessor, rel_mod)
 
     def _handleSystemJob(self, system, job):
+        self.lastJob = None
         # Validate event_uuid too - fetch SystemJob entry
         try:
             sj = job.systems.get(system__system_id=system.pk)
@@ -417,6 +424,8 @@ class SystemManager(BaseManager):
         job.time_updated = datetime.datetime.now(tz.tzutc())
         # XXX This just doesn't seem right
         job.save()
+        # Save the job so we know to update the system state
+        system.lastJob = job
 
 class ManagementNodeManager(SystemManager):
     """
