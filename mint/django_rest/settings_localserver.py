214e154954b3fb248d0d2aae375005517ef1e5c4
James Slagle <slagle@rpath.com>
2010-10-06 11:48:00 -0400

    some changes for running the django server locally

--- mint/django_rest/settings_localserver.py
+++ mint/django_rest/settings_localserver.py
@@ -29,12 +29,8 @@ AUTHENTICATION_BACKENDS = (
 MIDDLEWARE_CLASSES = tuple(
     x for x in MIDDLEWARE_CLASSES
         if x != 'mint.django_rest.middleware.SetMintConfigMiddleware')
-
-# Uncomment this to disable the comments middleware for local mode
-#MIDDLEWARE_CLASSES = tuple(
-#    x for x in MIDDLEWARE_CLASSES
-#        if x != 'mint.django_rest.middleware.AddCommentsMiddleware')
-# MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + ('mint.django_rest.middleware.LocalSetMintAdminMiddleware', )
+MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + \
+    ('mint.django_rest.middleware.LocalSetMintAdminMiddleware',)
 
 # Custom setting for if we should manage/create the tables in rbuilder.models
 MANAGE_RBUILDER_MODELS = True
