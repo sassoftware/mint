9b32fbd28bca5688bcc971a5a90f91e2ec9408c6
James Slagle <slagle@rpath.com>
2010-12-20 16:05:27 -0500

    Switch query param handling to ;, and also handle an optional ?_method

--- mint/django_rest/handler.py
+++ mint/django_rest/handler.py
@@ -4,6 +4,12 @@
 # All Rights Reserved
 #
 
+try:
+    # The mod_python version is more efficient, so try importing it first.
+    from mod_python.util import parse_qsl
+except ImportError:
+    from cgi import parse_qsl
+
 from django import http
 from django.core.handlers import modpython
 
@@ -11,12 +17,24 @@ class MintDjangoRequest(modpython.ModPythonRequest):
 
     def __init__(self, req):
         modpython.ModPythonRequest.__init__(self, req)
-        if ':' in self.path:
-            self.path, self.params = self.path.split(':')
-            self.path_info = self.path
-        else:
-            self.params = None
 
+        if '?' in self.path:
+            url, questionParams = self.path.split('?', 1)
+            questionParams = parse_qsl(questionParams)
+        else:
+            questionParams = []
+            url = self.path
+
+        if ';' in url:
+            self.path, semiColonParams = url.split(';', 1)
+            self.path_info = self.path
+            semiColonParams = parse_qsl(semiColonParams)
+        else:
+            semiColonParms = []
+
+        params = questionParams + semiColonParms
+        self.params = ['%s=%s' % (k, v) for k, v in params]
+        self.params = ';'.join(self.params)
 
     def _get_get(self):
         if not hasattr(self, '_get'):
--- mint/django_rest/middleware.py
+++ mint/django_rest/middleware.py
@@ -188,13 +188,26 @@ class RedirectMiddleware(redirectsmiddleware.RedirectFallbackMiddleware):
         nPRequest = NoParamsRequest(request)
         return redirectsmiddleware.RedirectFallbackMiddleware.process_response(self, nPRequest, response)
 
+
 class LocalQueryParameterMiddleware(object):
 
     def process_request(self, request):
-        path = request.get_full_path()
-        if ':' in path:
-            if path.endswith('/'):
-                path = path[:-1]
-            request.path, params = path.split(':')
+        if '?' in request.path:
+            url, questionParams = request.path.split('?', 1)
+            questionParams = parse_qsl(questionParams)
+        else:
+            questionParams = []
+            url = request.path
+
+        if ';' in url:
+            request.path, semiColonParams = url.split(';', 1)
             request.path_info = request.path
-            request.GET = http.QueryDict(params)
+            semiColonParams = parse_qsl(semiColonParams)
+
+        params = questionParams + semiColonParams
+        request.params = ['%s=%s' % (k, v) for k, v in params]
+        request.params = ';'.join(request.params)
+        method = request.GET.get('_method', None)
+        if method:
+            request.params += ';_method=%s' % method
+        request.GET = http.QueryDict(request.params)
