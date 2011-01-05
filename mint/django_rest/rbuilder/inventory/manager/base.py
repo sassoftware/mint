8a076af6465cfcf7b340742a2f4bb0dc6070b66b
James Slagle <slagle@rpath.com>
2011-01-05 10:33:16 -0500

    Fix ignore

--- mint/django_rest/rbuilder/inventory/manager/base.py
+++ mint/django_rest/rbuilder/inventory/manager/base.py
@@ -4,7 +4,7 @@
 # All Rights Reserved
 #
 
-from mint.django_rest.rbuilder.manager.basemanager import exposed # pyflakes=ignore
+from mint.django_rest.rbuilder.manager.basemanager import exposed  # pyflakes=ignore
 
 class BaseManager(object):
     def __init__(self, mgr):
