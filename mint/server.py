c3ad4b4c9639a67b8463bdbd8c6d150e6300b7ef
Michael Tharp <mtharp@rpath.com>
2011-01-14 10:41:58 -0500

    Send Windows bootable image builds to rMake; detect using proddef. (RBL-8073)

--- mint/server.py
+++ mint/server.py
@@ -3091,10 +3091,21 @@ If you would not like to be %s %s of this project, you may resign from this proj
             self.db.builds.update(buildId, status=jobstatus.FINISHED,
                     statusMessage="Job Finished")
             return '0' * 32
-        elif buildType in buildtypes.windowsBuildTypes:
-            return self.startWindowsImageJob(buildId)
         else:
             jobData = self.serializeBuild(buildId)
+            if buildType in buildtypes.windowsBuildTypes:
+                return self.startWindowsImageJob(buildId, jobData)
+
+            # Check the product definition to see if this is based on a Windows
+            # platform.
+            if buildDict['productVersionId'] is not None:
+                pd = self._getProductDefinitionForVersionObj(
+                        buildDict['productVersionId'])
+                tags = pd.getPlatformInformation(
+                        ).platformClassifier.tags.split()
+                if 'windows' in tags:
+                    return self.startWindowsImageJob(buildId, jobData)
+
             try:
                 client = self._getMcpClient()
                 uuid = client.new_job(client.LOCAL_RBUILDER, jobData)
@@ -3106,9 +3117,8 @@ If you would not like to be %s %s of this project, you may resign from this proj
                         statusMessage="Failed to start image job - check logs")
                 raise
 
-    def startWindowsImageJob(self, buildId):
+    def startWindowsImageJob(self, buildId, jobData):
         """Direct Windows image builds to rMake 3."""
-        jobData = self.serializeBuild(buildId)
         cli = wig_client.WigClient(self._getRmakeClient())
         job_uuid, job = cli.createJob(jobData, subscribe=False)
         log.info("Created Windows image job, UUID %s", job_uuid)
