8b8da188f6439b8a1d107675dd14f22af3a32637
James Slagle <slagle@rpath.com>
2011-01-24 11:53:41 -0500

    Add trailing_label property to package resource

--- mint/django_rest/rbuilder/packages/models.py
+++ mint/django_rest/rbuilder/packages/models.py
@@ -4,6 +4,8 @@
 # All Rights Reserved
 #
 
+from conary import versions
+
 from django.db import models
 from django.conf import settings
 
@@ -37,3 +39,10 @@ class Package(modellib.XObjIdModel):
     server_name = models.TextField(db_column="servername")
     branch_name = models.TextField(db_column="branchname")
     is_source = models.IntegerField(db_column="issource", default=0)
+
+    def serialize(self, *args, **kwargs):
+        xobjModel = modellib.XObjIdModel.serialize(self, *args, **kwargs)
+        trailingLabel = \
+            versions.VersionFromString(self.version).trailingLabel().asString()
+        xobjModel.trailing_label = trailingLabel
+        return xobjModel 
