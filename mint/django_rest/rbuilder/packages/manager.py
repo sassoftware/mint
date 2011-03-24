0c8fbbeba1fa4dd63459e2c15728e451cb3aa7a9
James Slagle <slagle@rpath.com>
2011-03-23 20:24:05 -0400

    Fix id's on package_source

--- mint/django_rest/rbuilder/packages/manager.py
+++ mint/django_rest/rbuilder/packages/manager.py
@@ -112,6 +112,7 @@ class PackageVersionManager(basemanager.BaseManager):
         packageSources = models.PackageSources()
         packageSources.package_source = list(
             models.PackageSource.objects.filter(package_version=packageVersion))
+        packageSources._parents = [packageVersion]
         return packageSources
 
     @exposed
