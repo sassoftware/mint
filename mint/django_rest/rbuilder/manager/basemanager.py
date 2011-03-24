cc304fcc1c06e8fcf04ccab021978923c1db0b12
James Slagle <slagle@rpath.com>
2011-03-24 09:44:16 -0400

    Copy of refactorings of managers from tech debt branch

--- mint/django_rest/rbuilder/manager/basemanager.py
+++ mint/django_rest/rbuilder/manager/basemanager.py
@@ -9,7 +9,7 @@ import logging
 from mint import config
 from mint import mint_error
 
-from mint.db.database import Database
+from mint.db import database
 from mint.django_rest.rbuilder import models as rbuildermodels
 from mint.rest.db.database import Database as RestDatabase
 
@@ -20,9 +20,35 @@ def exposed(fn):
     return fn
 
 class BaseManager(object):
+    def __init__(self, mgr):
+        # mgr is a weakref to avoid circular references. Access its fields
+        # through properties
+        self.mgr = mgr
+
+    @property
+    def cfg(self):
+        return self.mgr.cfg
+
+    @property
+    def restDb(self):
+        return self.mgr.restDb
+
+    @property
+    def user(self):
+        return self.mgr.user
+
+    @property
+    def auth(self):
+        return self.mgr._auth
+
+class BaseRbuilderManager(object):
+
+    MANAGERS = {}
+
     def __init__(self, cfg=None, userName=None):
         self.cfg = cfg
         self._auth = None
+        self.managers = []
         
         if not self.cfg:
             try:
@@ -40,28 +66,28 @@ class BaseManager(object):
             # The salt field contains binary data that blows django's little
             # mind when it tries to decode it as UTF-8. Since we don't need it
             # here, defer the loading of that column
-            self.user = rbuildermodels.Users.objects.defer("salt").get(username = userName)
+            self.user = rbuildermodels.Users.objects.defer("salt").get(username=userName)
 
-        # We instantiate _rest_db lazily
-        self._rest_db = None
+        # We instantiate _restDb lazily
+        self._restDb = None
 
     def setAuth(self, auth, user):
         self._auth = auth
         self.user = user
 
     @property
-    def rest_db(self):
+    def restDb(self):
         if self.cfg is None:
             return None
-        if self._rest_db is None:
+        if self._restDb is None:
             from django.conf import settings
             if settings.DATABASE_ENGINE == 'sqlite3':
                 self.cfg.dbPath = settings.DATABASE_NAME
             else:
                 self.cfg.dbPath = '%s:%s/%s' % (settings.DATABASE_HOST, 
                     settings.DATABASE_PORT, settings.DATABASE_NAME)
-            mint_db = Database(self.cfg)
-            self._rest_db = RestDatabase(self.cfg, mint_db)
+            mint_db = database.Database(self.cfg)
+            self._restDb = RestDatabase(self.cfg, mint_db)
             if self._auth:
-                self._rest_db.setAuth(self._auth, self._auth.getToken())
-        return self._rest_db
+                self._restDb.setAuth(self._auth, self._auth.getToken())
+        return self._restDb
