1f15cabd10831b7e3ad0837ed274a132657430f2
Mihai Ibanescu <https://issues.rpath.com/>
2011-03-25 01:03:15 -0400

    Specify path to download

--- mint/django_rest/rbuilder/packages/manager.py
+++ mint/django_rest/rbuilder/packages/manager.py
@@ -5,6 +5,7 @@
 #
 
 import datetime
+import tempfile
 from dateutil import tz
 
 from mint.django_rest.rbuilder.manager import basemanager
@@ -178,9 +179,19 @@ class PackageVersionManager(basemanager.BaseManager):
             return self._dispatchDownloadJob(package_version_job)
 
     def _dispatchDownloadJob(self, package_version_job):
+        # XXX we should use the config for this
+        destDir = "/srv/rbuilder/package-creator-downloads"
+        prefix = "pc-file-download-"
         urls = []
         for url in package_version_job.package_version.package_version_urls.all():
-            urls.append(rmakemodels.DownloadFile(url=url.url))
+            # Create a temporary file just to get a unique path. We'll close
+            # it immediately after that. The file will disappear, which is
+            # good, because rmake runs as user rmake while we're running as
+            # apache.
+            tmpf = tempfile.NamedTemporaryFile(dir=destDir, prefix=prefix)
+            path = tmpf.name
+            tmpf.close()
+            urls.append(rmakemodels.DownloadFile(url=url.url, path=path))
 
         repeaterClient = client.Client()
         resultsLocation = repeaterClient.ResultsLocation(
