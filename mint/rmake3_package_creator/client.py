78ce761ec8c1de6680d1fafb748d9d09ca80d58d
James Slagle <slagle@rpath.com>
2011-04-04 17:45:11 -0400

    Refactor download files and source commit into mint repeater client

--- mint/rmake3_package_creator/client.py
+++ mint/rmake3_package_creator/client.py
@@ -12,6 +12,11 @@
 # full details.
 #
 
+import tempfile
+
+from conary import conarycfg
+from conary import versions
+
 from rpath_repeater import client
 from mint.rmake3_package_creator import constants, models
 
@@ -19,14 +24,80 @@ class Client(client.RepeaterClient):
     def createJob(self, namespace, params):
         return self._createRmakeJob(namespace, params)
 
-    def pc_downloadFiles(self, params):
+    def pc_downloadFiles(self, urls, resultsLocation):
+        # XXX we should use the config for this
+        destDir = "/srv/rbuilder/package-creator-downloads"
+        prefix = "pc-file-download-"
+        urlList = []
+        for url in urls:
+            # Create a temporary file just to get a unique path. We'll close
+            # it immediately after that. The file will disappear, which is
+            # good, because rmake runs as user rmake while we're running as
+            # apache.
+            tmpf = tempfile.NamedTemporaryFile(dir=destDir, prefix=prefix)
+            path = tmpf.name
+            tmpf.close()
+            urlList.append(rmakemodels.DownloadFile(url=url, path=path))
+
+        params = rmakemodels.DownloadFilesParams()
+        params.urlList = urls
+        params.resultsLocation = self.ResultsLocation(path=resultsLocation)
+
         assert isinstance(params, models.DownloadFilesParams)
         assert isinstance(params.resultsLocation, self.ResultsLocation)
         if not isinstance(params.urlList, models.ImmutableList):
             params.urlList = models.ImmutableList(params.urlList)
         return self.createJob(constants.NS_JOB_DOWNLOAD_FILES, params)
 
-    def pc_packageSourceCommit(self, params):
+    def pc_packageSourceCommit(self, name, version, label, factory,
+                               user, password, email, resultsLocation):
+        conaryLabel = versions.Label(label)
+
+        cfg = conarycfg.ConaryConfiguration(readConfigFiles=False)
+        cfg.configLine('name Automatic Commit from rBuilder')
+        cfg.configLine('contact %s' % email)
+        cfg.configLine('buildLabel %s' % conaryLabel.asString())
+        # TODO: figure how to set the host
+        host = "https://127.0.0.1/conaryrc"
+        cfg.configLine('repositoryMap %s %s' % (conaryLabel.getHost(), host))
+        cfg.configLine('user * %s %s' % (user, password))
+
+        # TODO: figure how to set version, not sure we can require a Product
+        # definition if a goal is to be able to just commit to any old
+        # repository when you build a package.
+        pdl = models.ProductDefinitionLocation(
+            hostname=conaryLabel.getHost(),
+            shortname=conaryLabel.getHost().split('.')[0], 
+            namespace=conaryLabel.getNamespace(), 
+            version='1')
+
+        recipeContents = """
+        class OverrideRecipe(FactoryRecipeClass):
+            def preProcess(r):
+                '''This function is run at the beginning of setup'''
+            def postProcess(r):",
+                '''This function is run at the end of setup'''
+        """
+
+        mincfg = models.MinimalConaryConfiguration.fromConaryConfig(cfg)
+        mincfg.createConaryConfig().writeToFile("/tmp/conarycfg")
+
+        # TODO: again, not sure we can require stageLabel here
+        sourceData = rmakemodels.SourceData(name='%s:source' % name,
+            label=label, version=version,
+            productDefinitionLocation=pdl,
+            factory=factory,
+            stageLabel='devel', commitMessage="Committing\n",
+        )
+        sourceData.fileList = models.ImmutableList([
+            models.File(name="%s.recipe" % name, contents = recipeContents)
+        ])
+
+        params = models.PackageSourceCommitParams(
+            mincfg=mincfg, sourceData=sourceData)
+        resultsLocation = self.ResultsLocation(path=resultsLocation)
+        params.resultsLocation = resultsLocation
+
         assert isinstance(params, models.PackageSourceCommitParams)
         assert isinstance(params.resultsLocation, self.ResultsLocation)
         return self.createJob(constants.NS_JOB_COMMIT_SOURCE, params)
