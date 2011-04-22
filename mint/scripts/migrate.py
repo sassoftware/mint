cbf4734b81c7b8308375e1b3a7079c0af2973b52
Brad Murphy <https://issues.rpath.com>
2011-04-22 13:27:01 +0000

    fixed trunk mint schema, 57 now

--- mint/scripts/migrate.py
+++ mint/scripts/migrate.py
@@ -2585,6 +2585,18 @@ class MigrateTo_55(SchemaMigration):
     def migrate(self):
         return True
 
+class MigrateTo_56(SchemaMigration):
+    Version = (56, 0)
+
+    def migrate(self):
+        return True
+
+class MigrateTo_57(SchemaMigration):
+    Version = (57, 0)
+
+    def migrate(self):
+        return True
+
 
 #### SCHEMA MIGRATIONS END HERE #############################################
 
