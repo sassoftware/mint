8646c61736b681362d2a801a47ebefb893641277
Dan Cohn <http://issues.rpath.com/>
2011-05-17 14:09:38 +0000

    started adding managers

--- mint/django_rest/rbuilder/platforms/views.py
+++ mint/django_rest/rbuilder/platforms/views.py
@@ -12,7 +12,7 @@ class SourceStatusService(service.BaseService):
         return self.get(content_source_type, short_name)
 
     def get(self, content_source_type, short_name):
-        return self.mgr.getSourceStatus(content_source_type, short_name)
+        return self.mgr.getSourceStatusByName(content_source_type, short_name)
 
 
 class SourceErrorsService(service.BaseService):
@@ -22,14 +22,14 @@ class SourceErrorsService(service.BaseService):
 
     def get(self, content_source_type, short_name, error_id):
         if error_id:
-            return self.mgr.getSourceError(content_source_type, short_name, error_id)
+            return self.mgr.getPlatformContentError(content_source_type, short_name, error_id)
         else:
-            return self.mgr.getSourceErrors(content_source_type, short_name)
+            return self.mgr.getPlatformContentErrors(content_source_type, short_name)
 
     @return_xml
     @requires('resource_error')
     def rest_PUT(self, request, source_type, short_name, error_id, resource_error):
-        return self.mgr.updateSourceErrors(source_type, short_name, error_id, resource_error)
+        return self.mgr.updatePlatformContentError(source_type, short_name, error_id, resource_error)
         
 
 class SourceService(service.BaseService):
@@ -38,20 +38,23 @@ class SourceService(service.BaseService):
         return self.get(source_type, short_name)
         
     def get(self, source_type, short_name):
-        return self.mgr.getSource(source_type, short_name)
+        if short_name:
+            return self.mgr.getSource(short_name)
+        else:
+            return self.mgr.getSources(source_type)
 
     @return_xml
     @requires('source')
     def rest_PUT(self, request, source_type, short_name, source):
-        return self.mgr.updateSource(source_type, short_name, source)
+        return self.mgr.updateSource(short_name, source)
 
     @return_xml
     @requires('source')
     def rest_POST(self, request, source_type, source):
-        return self.mgr.createSource(source_type, source)
+        return self.mgr.createSource(source)
 
     def rest_DELETE(self, source_type, short_name):
-        self.mgr.deleteSource(source_type, short_name)
+        self.mgr.deleteSource(short_name)
         
 
 class SourceTypeDescriptorService(service.BaseService):
@@ -62,6 +65,13 @@ class SourceTypeDescriptorService(service.BaseService):
     def get(self, source_type):
         return self.mgr.getSourceTypeDescriptor(source_type)
     
+    
+class SourceTypeStatusTestService(service.BaseService):
+    @return_xml
+    @requires('source')
+    def rest_POST(self, request, source_type, source):
+        return self.mgr.getSourceStatus(source)
+
 
 class SourceTypeService(service.BaseService):
     @return_xml
@@ -69,7 +79,10 @@ class SourceTypeService(service.BaseService):
         return self.get(source_type)
         
     def get(self, source_type):
-        return self.mgr.getSourceType(source_type)
+        if source_type:
+            return self.mgr.getSourceType(source_type)
+        else:
+            return self.mgr.getSourceTypes()
 
 
 class PlatformStatusService(service.BaseService):
@@ -83,7 +96,7 @@ class PlatformStatusService(service.BaseService):
     @return_xml
     @requires('platform')
     def rest_POST(self, request, platform_id, platform):
-        return self.mgr.createPlatformStatus(platform_id, platform)
+        return self.mgr.getPlatformStatusTest(platform)
 
     
 class PlatformSourceService(service.BaseService):
@@ -92,7 +105,7 @@ class PlatformSourceService(service.BaseService):
         return self.get(platform_id)
         
     def get(self, platform_id):
-        return self.mgr.getPlatformSource(platform_id)
+        return self.mgr.getSourcesByPlatform(platform_id)
 
     
 class PlatformSourceTypeService(service.BaseService):
@@ -101,7 +114,7 @@ class PlatformSourceTypeService(service.BaseService):
         return self.get(platform_id)
         
     def get(self, platform_id):
-        return self.mgr.getPlatformSourceType(platform_id)
+        return self.mgr.getSourceTypesByPlatform(platform_id)
     
     
 class PlatformImageTypeService(service.BaseService):
@@ -110,16 +123,53 @@ class PlatformImageTypeService(service.BaseService):
         return self.get(platform_id)
         
     def get(self, platform_id):
-        return self.mgr.getPlatformImageType(platform_id)
+        return self.mgr.getPlatformImageTypeDefs(request, platform_id)
     
     
 class PlatformLoadService(service.BaseService):
-    pass
+    @return_xml
+    def rest_GET(self, request, platform_id, job_id):
+        return self.get(platform_id, job_id)
+        
+    def get(self, platform_id, job_id):
+        return self.mgr.getPlatformLoadStatus(platform_id, job_id)
+        
+    @return_xml
+    @requires('platform_load')
+    def rest_POST(self, request, platform_id, platform_load):
+        return self.mgr.loadPlatform(platform_id, platform_load)
     
     
 class PlatformVersionService(service.BaseService):
-    pass
+    @return_xml
+    def rest_GET(self, request, platform_id, platform_version_id=None):
+        return self.get(platform_id, platform_version_id)
     
+    def get(self, platform_id, platform_version_id):
+        if platform_version_id:
+            return self.mgr.getPlatformVersion(platform_id, platform_version_id)
+        else:
+            return self.mgr.getPlatformVersions(platform_id)
+
 
 class PlatformService(service.BaseService):
-    pass
+    @return_xml
+    def rest_GET(self, request, platform_id=None):
+        return self.get(platform_id)
+        
+    def get(self, platform_id):
+        if platform_id:
+            return self.mgr.getPlatform(platform_id)
+        else:
+            return self.mgr.getPlatforms()
+            
+    @return_xml
+    @requires('platform')
+    def rest_POST(self, request, platform):
+        platform_id = self.mgr.createPlatform(platform)
+        return self.mgr.getPlatform(platform_id)
+        
+    @return_xml
+    @requires('platform')
+    def rest_PUT(self, request, platform_id, platform):
+        return self.mgr.updatePlatform(platform_id, platform)
