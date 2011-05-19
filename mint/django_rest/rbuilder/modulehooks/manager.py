27cb3253b0c9dac26366f8156c760ec9ba23af29
Dan Cohn <http://issues.rpath.com/>
2011-05-19 18:32:48 +0000

    added modulehooks

---
+++ mint/django_rest/rbuilder/modulehooks/manager.py
@@ -0,0 +1,24 @@
+#
+# Copyright (c) 2011 rPath, Inc.
+#
+# All Rights Reserved
+#
+
+from mint.django_rest.rbuilder.manager import basemanager
+from mint.django_rest.rbuilder.modulehooks import models as modulehooksModels
+import os
+
+exposed = basemanager.exposed
+
+
+class ModuleHooksManager(basemanager.BaseManager):
+    @exposed
+    def getModuleHooks(self):
+        ModuleHooks = modulehooksModels.ModuleHooks()
+        ModuleHooks.module_hook = []
+        for path, dirs, files in os.walk(self.mgr.cfg.moduleHooksDir):
+            for file in [filename for filename in files]:
+                if fnmatch.fnmatch(filename, self.mgr.cfg.moduleHooksExt):
+                    joined = os.path.join('hooks', file)
+                    ModuleHooks.module_hook.append(modulehooksModels.ModuleHook(joined))
+        return ModuleHooks
