101b63b894cd197db7bd6d2931dacc868f6ef9c5
Dan Cohn <http://issues.rpath.com/>
2011-06-02 13:56:03 +0000

    minor param change to entry in schema.py

--- mint/django_rest/rFuzz.py
+++ mint/django_rest/rFuzz.py
@@ -8,7 +8,8 @@ def fuzzIt(module):
     for model in module.__dict__.values():
         if inspect.isclass(model):
             try:
-                fuzzCollection.append(Fuzzer(model).m)
+                if not model.Meta.abstract:
+                    fuzzCollection.append(Fuzzer(model).m)
                 print 'Success: %s' % model
             except Exception, e:
                 print 'Could not fuzz data %s: %s' % (model, e)
@@ -18,88 +19,11 @@ def fuzzIt(module):
 
     for fuzz in fuzzCollection:
         try:
-            fuzz.save()
-            print 'Saved: %s' % fuzz
+            if not fuzz.Meta.abstract:
+                fuzz.save()
+                print 'Saved: %s' % fuzz
         except Exception, e:
             print 'Could not save data %s: %s' % (fuzz, e) 
-
-
-# class Fuzzer(object):
-#     
-#     integers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-#     strings = ['abc', 'def', 'ghi', 'jkl', 'mno', 'pqr', 'stu']
-#     words = ['the', 'crazy', 'bird', 'walks', 'off', 'pier']
-#     
-#     REGISTRY = set()
-#         
-#     def __init__(self, model, skip_fields=None):
-#         self.skip_fields = skip_fields if skip_fields else []
-#         self.m = model()
-#         self.fields = dict((f.name, f) for f in model._meta.fields)
-#         for fname, field in self.fields.items():
-#             if isinstance(field, models.AutoField):
-#                 continue
-#             elif isinstance(field, (models.ForeignKey, models.ManyToManyField)):
-#                 if id(field.rel.get_related_field()) in [id(sf) for sf in self.skip_fields]:
-#                     continue
-#                 else:
-#                     data = self.dataFuzzer(field)
-#             elif self.m.Meta.abstract:
-#                 continue
-#             else:
-#                 data = self.dataFuzzer(field)
-#             setattr(self.m, fname, data)
-#             Fuzzer.REGISTRY.add(self.m)
-#         
-#     def dataFuzzer(self, field):
-#         if isinstance(field, models.CharField):
-#             return self.fuzzCharField()
-#         elif isinstance(field, models.IntegerField):
-#             return self.fuzzIntegerField()
-#         elif isinstance(field, models.TextField):
-#             return self.fuzzTextField()
-#         elif isinstance(field, models.BooleanField):
-#             return self.fuzzBooleanField()
-#         elif isinstance(field, models.ForeignKey):
-#             return self.fuzzForeignKeyField(field)
-#         elif isinstance(field, models.ManyToManyField):
-#             return self.fuzzManyToManyField(field)
-#         else:
-#             pass
-# 
-#     def fuzzCharField(self):
-#         return random.choice(Fuzzer.words)
-#         
-#     def fuzzIntegerField(self):
-#         return random.choice(Fuzzer.integers)
-#         
-#     def fuzzTextField(self):
-#         return random.choice(Fuzzer.words)
-#         
-#     def fuzzBooleanField(self):
-#         return random.choice([True, False])
-#         
-#     def fuzzForeignKeyField(self, field):
-#         related = field.related.model
-#         skipped = field.rel.get_related_field()
-#         if related not in Fuzzer.REGISTRY:
-#             import pdb; pdb.set_trace()
-#             fuzzed = Fuzzer(related, skip_fields=[skipped, related]).m
-#             Fuzzer.REGISTRY.add(fuzzed)
-#             return fuzzed
-#         
-#     def fuzzManyToManyField(self, field):
-#         import pdb; pdb.set_trace()
-#         pass
-        
-        
-class FuzzModel(object):
-    def __init__(self, model, *args, **kwargs):
-        self.m = model
-
-class FKFuzzModel(FuzzModel):
-    def __init__(self, model, *args, **kwargs):
-        FuzzModel.__init__(self, model, *args, **kwargs)
 
 
 class Fuzzer(object):
@@ -117,12 +41,11 @@ class Fuzzer(object):
         for fname, field in self.fields.items():
             hsh = id(self.m)
             if isinstance(field, (models.ForeignKey, models.ManyToManyField)):
-                # if field.name == 'load':
-                #     import pdb; pdb.set_trace()
                 if hsh in Fuzzer.FKREGISTRY:
                     continue
                 else:
-                    self.fuzzForeignKeyField(field)
+                    instance = self.fuzzForeignKeyField(field)
+                    Fuzzer.FKREGISTRY[id(self.m)] = (self.m, field.name, instance)
                 setattr(*Fuzzer.FKREGISTRY[hsh])
             else:
                 data = self.fuzzData(field)
@@ -164,8 +87,7 @@ class Fuzzer(object):
 
     def fuzzForeignKeyField(self, field):
         related = field.related.parent_model
-        instance = Fuzzer(related).m
-        Fuzzer.FKREGISTRY[id(self.m)] = (self.m, field.name, instance)
+        return Fuzzer(related).m
 
     def fuzzManyToManyField(self, field):
         import pdb; pdb.set_trace()
