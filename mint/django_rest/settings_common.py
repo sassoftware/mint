993fc69de022644052cfa317c079e5008fd26ba5
Mihai Ibanescu <https://issues.rpath.com/>
2011-07-07 16:18:08 -0400

    Silencing a django warning

--- mint/django_rest/settings_common.py
+++ mint/django_rest/settings_common.py
@@ -56,8 +56,8 @@ SECRET_KEY = 'o5pks8b=o8zmpr%nu=4$##n*o_6s(t%r%i%h)*(9w)#yg=9*4u'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
