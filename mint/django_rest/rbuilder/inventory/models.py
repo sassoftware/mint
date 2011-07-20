8834ff866c48dfa754389fc782e78348c76264d8
Mihai Ibanescu <https://issues.rpath.com/>
2011-07-20 14:51:06 -0400

    s/event_type/job_type/cg

--- mint/django_rest/rbuilder/inventory/models.py
+++ mint/django_rest/rbuilder/inventory/models.py
@@ -885,7 +885,7 @@ class SystemEvent(modellib.XObjIdModel):
     system_event_id = models.AutoField(primary_key=True)
     system = modellib.DeferredForeignKey(System, db_index=True,
         related_name='system_events')
-    event_type = modellib.DeferredForeignKey(jobmodels.EventType,
+    job_type = modellib.DeferredForeignKey(jobmodels.EventType,
         related_name='system_events')
     time_created = modellib.DateTimeUtcField(auto_now_add=True)
     time_enabled = modellib.DateTimeUtcField(
@@ -894,7 +894,7 @@ class SystemEvent(modellib.XObjIdModel):
     event_data = models.TextField(null=True)
 
     def dispatchImmediately(self):
-        return self.event_type.priority >= jobmodels.EventType.ON_DEMAND_BASE
+        return self.job_type.priority >= jobmodels.EventType.ON_DEMAND_BASE
 
     def get_absolute_url(self, request, parents=None, *args, **kwargs):
         if parents:
@@ -907,7 +907,7 @@ class SystemEvent(modellib.XObjIdModel):
 
     def save(self, *args, **kw):
         if not self.priority:
-            self.priority = self.event_type.priority
+            self.priority = self.job_type.priority
         modellib.XObjIdModel.save(self, *args, **kw)
 
 class Network(modellib.XObjIdModel):
