2db9f682428081e07ba0cf7ec5e94eab198f1299
Prathibha Ayyappan <https://issues.rpath.com/>
2011-07-21 12:37:00 -0400

    fixed testsxml.py in inventory

--- mint/django_rest/rbuilder/inventory/testsxml.py
+++ mint/django_rest/rbuilder/inventory/testsxml.py
@@ -2124,10 +2124,10 @@ system_available_updates_xml = """\
   <local_uuid>testsystemlocaluuid</local_uuid>
   <major_version/>
   <management_interface id="http://testserver/api/v1/inventory/management_interfaces/1">Common Information Model (CIM)</management_interface>
-  <current_state id="http://testserver/api/v1/inventory/system_states/2">
+  <current_state id="http://testserver/api/v1/inventory/system_states/3">
     <description>Initial synchronization pending</description>
     <name>registered</name>
-    <system_state_id>2</system_state_id>
+    <system_state_id>3</system_state_id>
   </current_state>
   <created_date>2010-08-27T12:21:59.800269+00:00</created_date>
 </system>
