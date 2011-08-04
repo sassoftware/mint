6f9e25596e10cadd14b0b711e90bb7cfb9a3f94d
Michael DeHaan <mdehaan@rpath.com>
2011-08-04 10:13:47 -0400

    Stubbed out tests for RBAC, starting on models.

--- mint/django_rest/rbuilder/rbac/models.py
+++ mint/django_rest/rbuilder/rbac/models.py
@@ -7,7 +7,7 @@
 # RBAC models
 
 #import datetime
-#import sys
+import sys
 #import urllib
 #import urlparse
 #from dateutil import tz
@@ -16,43 +16,84 @@
 #from conary.deps import deps
 #
 #from django.conf import settings
-#from django.db import connection, models
+from django.db import connection, models
 #from django.db.backends import signals
-#from mint.django_rest.deco import D
-#from mint.django_rest.rbuilder import modellib
+from mint.django_rest.deco import D
+from mint.django_rest.rbuilder import modellib
 #from mint.django_rest.rbuilder import models as rbuildermodels
 #from mint.django_rest.rbuilder.projects.models import Project, ProjectVersion, Stage
 #from mint.django_rest.rbuilder.users import models as usersmodels
 #from mint.django_rest.rbuilder.jobs import models as jobmodels
-#from xobj import xobj
+from xobj import xobj
 #
 #Cache = modellib.Cache
 #XObjHidden = modellib.XObjHidden
 #APIReadOnly = modellib.APIReadOnly
 
-class RbacRoles(object):
-    pass
+class RbacRoles(modellib.XObjModel):
+    '''
+    A collection of RbacRoles
+    '''
 
-class RbacRole(object):
-    pass
+    # XSL = 'fixme.xsl' # TODO
+    class Meta:
+        abstract = True
+    _xobj = xobj.XObjMetadata(tag = 'rbac_roles')
+    list_fields = ['rbac_role']
+    role = []
+    objects = modellib.RbacRolesManager() # TODO: add django manager
+    view_name = 'RbacRoles' # TODO: add view
+
+    def __init__(self):
+        modellib.Collection.__init__(self)
+
+    def save(self):
+        return [s.save() for s in self.role]
+
+class RbacRole(modellib.XObjIdModel):
+    '''
+    An RbacRole represents a role a user has that is used to determine a RbacPermission
+    on an RbacContext.  Example roles could be "syadmin", "developer", or "it_architect"
+    '''
+
+    # XSL = "fixme.xsl" # TODO
+
+    class Meta:
+        db_table = 'rbac_role'
+
+    view_name = 'RbacRole' # TODO
+
+    _xobj = xobj.XObjMetadata(
+        tag = 'rbac_role',
+        attributes = {'id':str},
+    )
+
+    # objects = modellib.RbacManager() # NEEDED?
+
+    role_id = D(models.TextField(primary_key=True),
+        "the database ID for the role")
 
 class RbacContexts(object):
+    # TODO
     pass
 
 class RbacContext(object):
+    # TODO
     pass
 
 class RbacPermissions(object):
+    # TODO
     pass
 
 class RbacPermission(object):
+    # TODO
     pass
 
  
-#for mod_obj in sys.modules[__name__].__dict__.values():
-#    if hasattr(mod_obj, '_xobj'):
-#        if mod_obj._xobj.tag:
-#            modellib.type_map[mod_obj._xobj.tag] = mod_obj
+for mod_obj in sys.modules[__name__].__dict__.values():
+    if hasattr(mod_obj, '_xobj'):
+        if mod_obj._xobj.tag:
+            modellib.type_map[mod_obj._xobj.tag] = mod_obj
 #for mod_obj in rbuildermodels.__dict__.values():
 #    if hasattr(mod_obj, '_meta'):
 #        modellib.type_map[mod_obj._meta.verbose_name] = mod_obj
--- mint/django_rest/rbuilder/rbac/tests.py
+++ mint/django_rest/rbuilder/rbac/tests.py
@@ -34,13 +34,98 @@ XMLTestCase = test_utils.XMLTestCase
 
 class RbacBasicTestCase(XMLTestCase):
 
-    def setUp(self):
-        # just a stub for later...
-        XMLTestCase.setUp(self)
-        a = models.RbacContext()
-        b = models.RbacRole()
-        c = models.RbacPermission()
+    #def setUp(self):
+    #    # just a stub for later...
+    #    XMLTestCase.setUp(self)
+    #    pass
+
+    def testModelsForRbacRoles(self):
+        '''verify django models for roles work'''
+
+        sysadmin = models.RbacRole(pk='sysadmin')
+        sysadmin.save()
+        developer = models.RbacRole(pk='developer')
+        developer.save()
+
+        self.assertEquals(len(models.RbacRole.objects.all()), 2,
+            'correct number of results'
+        )
+
+        developer2 = models.RbacRole.objects.get(pk='developer')
+        self.assertEquals(developer2.role_id, 'developer', 
+            'object fetched correctly'
+        )
+
+    def testModelsForRbacContexts(self):
         pass
 
-    def testFoo(self):
+    def testModelsForRbacPermissions(self):
         pass
+
+    def testModelsForUserRoleAssignment(self):
+        pass
+
+    def testModelsForSystemContextAssignment(self):
+        pass
+
+class RbacRoleViews(XMLTestCase):
+    def testCanListRoles(self):
+        pass
+    def testCanAddRoles(self):
+        pass
+    def testCanDeleteRoles(self):
+        pass
+    def testCanUpdateRoles(self):
+        pass
+
+class RbacPermissionViews(XMLTestCase):
+    def testCanListPermissions(self):
+        pass
+    def testCanAddPermissions(self):
+        pass
+    def testCanDeletePermissions(self):
+        pass
+    def testCanUpdatePermissions(self):
+        pass
+
+class RbacContextViews(XMLTestCase):
+    def testCanListContexts(self):
+        pass
+    def testCanAddContexts(self):
+        pass
+    def testCanDeleteContexts(self):
+        pass
+    def testCanUpdatePermissions(self):
+        pass
+
+class RbacUserViewTests(XMLTestCase):
+    def testCanAssignUserToRole(self):
+        pass
+    def testCanRemoveUserRole(self):
+        pass
+   
+class RbacSystemViewTests(XMLTestCase):
+    def testCanAssignSystemToContext(self):
+        pass
+    def testCanRemoveSystemContext(self):
+        pass
+
+class AccessControlSystemTests(XMLTestCase):
+    # inventory tests will also help cover this
+    # may want to add AccessControl tests there instead (probably do)
+    def testAdminsCanAccessSystemWithContext(self):
+       pass
+    def testAdminsCanAccessSystemWithoutContext(self):
+       pass
+    def testUserCanAccessSystemWithContext(self):
+       pass
+    def testUserCannotAccessSystemWithWrongContext(self):
+       pass
+    def testUserCanAccessSystemWithoutContext(self):
+       pass
+
+class AccessControlImageTests(XMLTestCase):
+    pass
+
+class AccessControlPlatformTests(XMLTestCase):
+    pass
