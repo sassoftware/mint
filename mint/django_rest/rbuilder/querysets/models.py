9b316715fd9c3f579af3ad34afbe2bd4c7c297c3
Michael DeHaan <mdehaan@rpath.com>
2011-08-30 09:47:35 -0400

    Hide some internal relations from the queryset serialized model

--- mint/django_rest/rbuilder/querysets/models.py
+++ mint/django_rest/rbuilder/querysets/models.py
@@ -84,11 +84,13 @@ class QuerySet(modellib.XObjIdModel):
     _xobj = xobj.XObjMetadata(
                 tag = "query_set")
     _xobj_hidden_accessors = set([
-        'rbacpermission_set', 
+        'rbacpermission_set',
         'stage_tags',
         'system_tags',
         'user_tags',
-        'project_tags'
+        'project_tags',
+        'role_tags',
+        'permission_tags'
     ])
 
     query_set_id = D(models.AutoField(primary_key=True),
