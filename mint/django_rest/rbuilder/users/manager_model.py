735f4af35e33a4b1c2628e07f0bdd31d66fba198
Mihai Ibanescu <https://issues.rpath.com/>
2011-09-14 11:20:56 -0400

    Do not save user object, to preserve the previous behavior

--- mint/django_rest/rbuilder/users/manager_model.py
+++ mint/django_rest/rbuilder/users/manager_model.py
@@ -10,6 +10,5 @@ class UserManager(basemodels.BaseManager):
     def load_from_object(self, xobjModel, request, flags=None):
         # We absolutely don't want the model to be saved, we'll let the
         # old rbuilder interface handle the saving
-        save = False
         return basemodels.BaseManager.load_from_object(self,
-            xobjModel, request, flags=flags)
+            xobjModel, request, flags=flags.copy(save=False))
