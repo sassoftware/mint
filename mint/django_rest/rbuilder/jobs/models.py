04dd15e860233deb9751854b4b963daf630cc69f
Mihai Ibanescu <https://issues.rpath.com/>
2011-09-23 12:36:31 -0400

    pyflakes

--- mint/django_rest/rbuilder/jobs/models.py
+++ mint/django_rest/rbuilder/jobs/models.py
@@ -10,7 +10,6 @@ from django.db import models
 from mint.django_rest.deco import D
 from mint.django_rest.rbuilder import modellib
 from mint.django_rest.rbuilder.users import models as usermodels
-import urlparse
 from xobj import xobj
 
 XObjHidden = modellib.XObjHidden
