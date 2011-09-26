a66deb4f61d778ae7670ec159b62c31a2d861ae7
Dan Cohn <http://issues.rpath.com/>
2011-09-26 14:01:09 +0000

    built skeletons for builds/publishedreleases/etc models inside images

--- mint/django_rest/rbuilder/images/models.py
+++ mint/django_rest/rbuilder/images/models.py
@@ -64,4 +64,88 @@ class Image(modellib.XObjIdModel):
     name = D(models.TextField(), 
         "the database ID for the permission")
 
+class PublishedReleases(modellib.Collection):
+    class Meta:
+        abstract = True
+    _xobj = xobj.XObjMetadata(tag='published_releases')
+    list_fields = ['published_release']
+    
+    
+class PublishedRelease(modellib.XObjIdModel):
+    class Meta:
+        db_table = 'PublishedReleases'
+        
+    pub_release_id = models.AutoField(primary_key=True, db_column='pubreleaseid')
+    project_id = models.ForeignKey('projects.Project', null=False, db_column='projectid')
+    name = models.CharField(max_length=255, null=False, default='')
+    version = models.CharField(max_length=32, null=False, default='')
+    description = models.TextField()
+    time_created = modellib.DecimalField(max_digits=14, decimal_places=3, db_column='timecreated')
+    created_by = models.ForeignKey('users.User', db_column='createdby')
+    time_updated = modellib.DecimalField(max_digits=14, decimal_places=3, db_column='timeupdated')
+    updated_by = models.ForeignKey('users.User', db_column='updatedby')
+    time_published = modellib.DecimalField(max_digits=14, decimal_places=3, db_column='timepublished')
+    published_by = models.ForeignKey('users.User', db_column='publishedby')
+    should_mirror = models.IntegerField(null=False, default=0, db_column='shouldmirror')
+    time_mirrored = modellib.DecimalField(max_digits=14, decimal_places=3, db_column='timemirrored')
 
+
+class Builds(modellib.XObjCollection):
+    class Meta:
+        abstract = True
+        
+    list_fields = ['build']
+    
+
+class Build(modellib.XObjIdModel):
+    class Meta:
+        db_table = 'builds'
+    
+    build_id = models.AutoField(primary_key=True, db_column='buildid')
+    project_id = models.ForeignKey('projects.Project', db_column='projectid', null=False)
+    stage_id = models.ForeignKey('projects.Stage')
+    pub_release_id = models.ForeignKey('PublishedRelease', db_column='pubreleaseid')
+    build_type = models.IntegerField(db_column='buildtype')
+    job_uuid = models.IntegerField() # is of type 'uuid' in schema
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    trove_name = models.CharField(max_length=128, db_column='trovename')
+    trove_version = models.CharField(max_length=255, db_column='troveversion')
+    trove_flavor = models.CharField(max_length=4096, db_column='troveflavor')
+    trove_last_changed = modellib.DecimalField(
+        max_digits=14, decimal_places=3, db_column='trovelastchanged')
+    time_created = modellib.DateTimeUtcField(
+        max_digits=14, decimal_places=3, db_column='timecreated')
+    created_by = models.ForeignKey('users.User', db_column='createdby')
+    time_updated = modellib.DecimalField(
+        max_digits=14, decimal_places=3, db_column='timeupdated')
+    updated_by = models.ForeignKey('users.User', db_column='updatedby')
+    build_count = models.IntegerField(null=False, default=0, db_column='buildcount')
+    product_version_id = models.ForeignKey('projects.ProjectVersion', db_column='productversionid')
+    stage_name = models.CharField(max_length=255, default='', db_column='stagename')
+    status = models.IntegerField(default=-1)
+    status_message = models.TextField(default='', db_column='statusmessage')
+    output_trove = models.TextField()
+
+
+
+class BuildFiles(modellib.XObjCollection):
+    class Meta:
+        abstract = True
+        
+    _xobj = xobj.XObjMetadata(tag='build_files')
+    list_fields = 'build_file'
+    
+    
+class BuildFile(modellib.XObjIdModel):
+    class Meta:
+        db_table = 'buildfiles'
+        
+    file_id = models.AutoField(primary_key=True, db_column='fileid')
+    build_id = models.ForeignKey('Build', null=False, db_column='buildid')
+    idx = models.IntegerField(null=False, default=0)
+    title = models.CharField(max_length=255, null=False, default='')
+    size = models.IntegerField()
+    sha1 = models.CharField(max_length=40)
+    
+    
