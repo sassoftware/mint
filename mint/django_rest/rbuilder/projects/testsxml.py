cd94075ef41d12537145f77485203d0db96ac0d4
Michael DeHaan <mdehaan@rpath.com>
2011-11-01 18:27:42 -0400

    Remove some relations that won't scale well -- if the same project is used to deploy 5000 systems, and we get 100 projects, we don't want the XML for 500,000 systems in one page.
    (This has the side effect of making filter term dynamic building much more
    logical as to the choices it offers the user)

--- mint/django_rest/rbuilder/projects/testsxml.py
+++ mint/django_rest/rbuilder/projects/testsxml.py
@@ -213,7 +213,6 @@ project_branch_stage_xml = """\
   </project_branch>
   <promotable>false</promotable>
   <stage_id>12</stage_id>
-  <systems/>
 </project_branch_stage>"""
 
 project_branch_stage_put_xml = \
@@ -233,7 +232,6 @@ project_branch_stage_put_xml = \
   </project_branch>
   <promotable>false</promotable>
   <stage_id>12</stage_id>
-  <systems/>
 </stage>
 """.strip()
 
@@ -282,7 +280,6 @@ project_branch_stages_xml = \
       <short_name>chater-foo</short_name>
       <name>chater-foo</name>
     </project>
-    <systems/>
     <groups promote_href="https://testserver/api/products/chater-foo/versions/trunk/stages/Development" href="https://testserver/api/products/chater-foo/repos/search?type=group&amp;label=foo@ns:trunk-devel"/>
     <created_date>2011-10-12T21:22:24.206535+00:00</created_date>
     <images id="http://testserver/api/v1/projects/chater-foo/project_branches/chater-foo.eng.rpath.com@rpath:chater-foo-trunk/project_branch_stages/Development/images"/>
@@ -301,7 +298,6 @@ project_branch_stages_xml = \
       <short_name>chater-foo</short_name>
       <name>chater-foo</name>
     </project>
-    <systems/>
     <groups promote_href="https://testserver/api/products/chater-foo/versions/trunk/stages/QA" href="https://testserver/api/products/chater-foo/repos/search?type=group&amp;label=foo@ns:trunk-qa"/>
     <created_date>2011-10-12T21:22:24.209524+00:00</created_date>
     <images id="http://testserver/api/v1/projects/chater-foo/project_branches/chater-foo.eng.rpath.com@rpath:chater-foo-trunk/project_branch_stages/QA/images"/>
@@ -320,7 +316,6 @@ project_branch_stages_xml = \
       <short_name>chater-foo</short_name>
       <name>chater-foo</name>
     </project>
-    <systems/>
     <groups promote_href="https://testserver/api/products/chater-foo/versions/trunk/stages/Stage" href="https://testserver/api/products/chater-foo/repos/search?type=group&amp;label=foo@ns:trunk-stage"/>
     <created_date>2011-10-12T21:22:24.212755+00:00</created_date>
     <images id="http://testserver/api/v1/projects/chater-foo/project_branches/chater-foo.eng.rpath.com@rpath:chater-foo-trunk/project_branch_stages/Stage/images"/>
@@ -339,7 +334,6 @@ project_branch_stages_xml = \
       <short_name>chater-foo</short_name>
       <name>chater-foo</name>
     </project>
-    <systems/>
     <groups promote_href="https://testserver/api/products/chater-foo/versions/trunk/stages/Release" href="https://testserver/api/products/chater-foo/repos/search?type=group&amp;label=foo@ns:trunk"/>
     <created_date>2011-10-12T21:22:24.215180+00:00</created_date>
     <images id="http://testserver/api/v1/projects/chater-foo/project_branches/chater-foo.eng.rpath.com@rpath:chater-foo-trunk/project_branch_stages/Release/images"/>
