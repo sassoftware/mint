bebde23455714223156f82df74094765aa0ca15c
Michael DeHaan <mdehaan@rpath.com>
2012-02-01 11:03:01 -0500

    Initial basic Django models for sysdisco.

--- mint/django_rest/rbuilder/inventory/survey_models.py
+++ mint/django_rest/rbuilder/inventory/survey_models.py
@@ -19,7 +19,7 @@ class Surveys(modellib.Collection):
         abstract = True
     _xobj = xobj.XObjMetadata(tag = 'surveys')
     list_fields = ['survey']
-    system = []
+    survey = []
     view_name = 'Surveys'
 
     def __init__(self):
@@ -29,71 +29,123 @@ class Surveys(modellib.Collection):
         # shouldn't really be surfacing this
         return [s.save() for s in self.survey]
 
+class Diffs(modellib.Collection):
+    class Meta:
+        abstract = True
+
+    _xobj = xobj.XObjMetadata(tag='survey_diffs')
+    list_fields = [ 'survey_diff']
+    survey_diff = []
+    view_name = 'SurveyDiffs'
+ 
+    def __init__(self):
+        modellib.Collection.__init__(self)
+
+    def save(self):
+        return [s.save() for s in self.survey_diffs]
+
 class Survey(modellib.XObjIdModel):
     
     class Meta:
         db_table = 'inventory_survey'
     view_name = 'Survey'
-    _xobj_explicit_accessors = set([])
+    _xobj_explicit_accessors = set([
+         'conary_packages',
+         'rpm_packages',
+         'services',
+         'tags',
+    ])
     _xobj = xobj.XObjMetadata(
-                tag = 'survey',
-                attributes = {'id':str})
+        tag = 'survey', attributes = {'id':str}
+    )
 
     survey_id    = D(models.AutoField(primary_key=True),
         "the database ID for the survey", short="Survey ID")
-    tags         = models.ManyToManyField('SurveyTag', through="SurveyTags")
     name         = models.TextField()
+    uuid         = models.TextField(null=False)
     description  = models.TextField()
     created_date = modellib.DateTimeUtcField(auto_now_add=True)
     removeable   = models.BooleanField(default=True)
     comment      = models.TextField()
 
+    # FIXME: TODO: custom URL method so URL is UUID based
+
+
+class RpmPackage(modellib.XObjIdModel):
+     class Meta:
+         db_table = 'inventory_rpm_package'
+     _xobj = xobj.XObjMetadata(tag='_rpm_package')
+     rpm_package_id = models.AutoField(primary_key=True)
+     name           = models.TextField(null=False)
+     epoch          = models.IntegerField(null=True)
+     version        = models.TextField(null=False)
+     release        = models.TextField(null=False)
+     architecture   = models.TextField(null=False)
+     description    = models.TextField(null=True)
+     signature      = models.TextField(null=False)
+
+class ConaryPackage(modellib.XObjIdModel):
+    class Meta:
+        db_table = 'inventory_conary_package'
+    _xobj = xobj.XObjMetadata(tag='_conary_package')
+    conary_package_id = models.AutoField(primary_key=True)
+    name              = models.TextField(null=False)
+    version           = models.TextField(null=False)
+    flavor            = models.TextField(null=False)
+    description       = models.TextField(null=False)
+    revision          = models.TextField(null=False)
+    architecture      = models.TextField(null=False)
+    signature         = models.TextField(null=False)
+    rpm_package       = modellib.DeferredForeignKey(RpmPackage, related_name='+')
+
+class Service(modellib.XObjIdModel):
+    class Meta:
+        db_table = 'inventory_service'
+    _xobj = xobj.XObjMetadata(tag='_service')
+
+    service_id        = models.AutoField(primary_key=True)
+    name              = models.TextField(null=False)
+    autostart         = models.BooleanField(default=False)
+    runlevels         = models.TextField(default='')
+
 class SurveyTag(modellib.XObjIdModel):
 
     class Meta:
-       db_table = 'inventory_survey_tag'
+       db_table = 'inventory_survey_tags'
     _xobj = xobj.XObjMetadata(tag='tag')
 
-    survey_tag_id = models.AutoField(primary_key=True)
-    tag = models.TextField()
+    tag_id = models.AutoField(primary_key=True)
+    survey = modellib.ForeignKey(Survey, related_name='tags')
+    name = models.TextField()
 
-class SurveyTags(modellib.XObjIdModel):
+class SurveyRpmPackage(modellib.XObjIdModel):
     class Meta:
-        db_table = 'inventory_survey_tags'
-    # XXX This class should never be serialized directly, but unless an _xobj
-    # field is added, we have no access to it from modellib
-    _xobj = xobj.XObjMetadata(tag='__surveyTags')
-    survey_tags_id = models.AutoField(primary_key=True)
-    survey = modellib.ForeignKey(Survey)
-    tag = models.ForeignKey('SurveyTag', unique=True, related_name='surveys')
-
-#class SurveyRpmPackage(modellib.XObjIdModel):
-#    class Meta:
-#        db_table = 'inventory_survey_rpm_package'
-#    survey_id     = models.AutoField(primary_key=True)
-#    rpm_package   =
-#    install_date  = mdoellib.DateTimeUtcField(auto_now_add=True)
+        db_table = 'inventory_survey_rpm_package'
+    _xobj = xobj.XObjMetadata(tag='rpm_package')
+    map_id        = models.AutoField(primary_key=True)
+    survey_id     = modellib.ForeignKey(Survey)
+    rpm_package   = modellib.ForeignKey(RpmPackage, related_name='rpm_packages')
+    install_date  = modellib.DateTimeUtcField(auto_now_add=True)
     
 
-#class SurveyConaryPackage(modellib.XObjIdModel):
-#    class Meta:
-#        db_table = 'inventory_survey_conary_package'
-
-#class SurveyService(modellib.XObjIdModel):
-#    class Meta:
-#        db_table = 'inventory_survey_service'
-
-#class RpmPackage(modellib.XObjIdModel):
-#    class Meta:
-#        db_table = 'inventory_conary_package'
-
-#class ConaryPackage(modellib.XObjIdModel):
-#    class Meta:
-#        db_table = 'inventory_conary_package'
-
-#class Service(modellib.XObjIdModel):
-#    class Meta:
-#        db_table = 'inventory_service'
+class SurveyConaryPackage(modellib.XObjIdModel):
+    class Meta:
+        db_table = 'inventory_survey_conary_package'
+    _xobj = xobj.XObjMetadata(tag='conary_package')
+    map_id         = models.AutoField(primary_key=True)
+    survey_id      = modellib.ForeignKey(Survey)
+    conary_package = modellib.ForeignKey(ConaryPackage, related_name='conary_packages')
+    install_date   = modellib.DateTimeUtcField(auto_now_add=True)
+ 
+class SurveyService(modellib.XObjIdModel):
+    class Meta:
+        db_table = 'inventory_survey_service'
+    _xobj = xobj.XObjMetadata(tag='service')
+    map_id         = models.AutoField(primary_key=True)
+    survey_id      = modellib.ForeignKey(Survey)
+    service        = modellib.ForeignKey(Service, related_name='services')
+    running        = models.BooleanField()
+    status         = models.TextField()
 
 for mod_obj in sys.modules[__name__].__dict__.values():
     if hasattr(mod_obj, '_xobj'):
