8b6241f88364ccebe5501e47c3755ff03a02d575
Michael Tharp <mtharp@rpath.com>
2012-08-28 18:22:40 +0000

    Parse fqdn for preload from the provided metadata (RCE-731)

--- mint/scripts/restore_preload_bundle.py
+++ mint/scripts/restore_preload_bundle.py
@@ -3,6 +3,7 @@ import logging
 import os
 import pwd
 import sys
+import tarfile
 from mint.db import database
 from mint.lib import scriptlibrary
 from mint.rest.db import database as restdb
@@ -18,16 +19,46 @@ class Script(scriptlibrary.GenericScript):
     def action(self):
         self.loadConfig()
         self.resetLogging(verbose=True)
-        if len(sys.argv) < 3:
-            sys.exit("usage: %s <fqdn> <dump file>" % sys.argv[0])
-        fqdn, dump = sys.argv[1:]
+        args = sys.argv[1:]
+        if len(args) == 1:
+            dump = args[0]
+            fqdn = None
+        elif len(args) == 2:
+            fqdn, dump = args
+        else:
+            sys.exit("usage: %s <dump file>" % sys.argv[0])
 
+        # Extract actual FQDN from the metadata in the preload
+        tar = tarfile.open(dump)
+        tarm = tar.next()
+        if tarm.name not in ('metadata', './metadata'):
+            sys.exit("Invalid preload tarball: "
+                    "first member must be named 'metadata'")
+        for line in tar.extractfile(tarm):
+            key, value = line.rstrip().split(None, 1)
+            if key == 'serverName':
+                fqdn2 = value
+                break
+        else:
+            sys.exit("Invalid preload tarball: "
+                    "missing metadata field 'serverName'")
+        tar.close()
+
+        # If the user provided a FQDN, make sure it matches
+        if fqdn and fqdn != fqdn2:
+            sys.exit("Preload FQDN %s does not match requested FQDN %s" %
+                    (fqdn2, fqdn))
+        elif not fqdn:
+            fqdn = fqdn2
+
+        # Drop privileges to make sure created files are owned by apache
         if os.getuid() == 0:
             pwnam = pwd.getpwnam('apache')
             os.setgid(pwnam.pw_gid)
             os.setuid(pwnam.pw_uid)
             log.info("Dropped privileges to uid %d", pwnam.pw_uid)
 
+        # Get a repository handle to restore to
         db = database.Database(self.cfg)
         restDb = restdb.Database(self.cfg, db)
         try:
@@ -53,6 +84,7 @@ class Script(scriptlibrary.GenericScript):
             restDb.platformMgr.updatePlatform(platform.platformId, platform)
             handle = restDb.reposShim.getRepositoryFromFQDN(fqdn)
 
+        # Do the restore, create helper roles, and nuke caches
         handle.restoreBundle(dump, replaceExisting=True)
 
         restDb.productMgr.reposMgr.populateUsers(handle)
