8905ef06d29229c7ab22f5290e4ff70588cba375
Michael Tharp <michael.tharp@sas.com>
2013-08-22 13:30:05 +0000

    Fix scoping error in puppet module

--- ha/rpath_ha/manifests/managed_service.pp
+++ ha/rpath_ha/manifests/managed_service.pp
@@ -1,6 +1,6 @@
 define rpath_ha::managed_service ($service=$title) {
     cs_primitive { $title:
-        cib                         => $cib,
+        cib                         => $rpath_ha::cib,
         primitive_class             => 'lsb',
         primitive_type              => $service,
         parameters                  => {},
