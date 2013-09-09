824520e34fe9ad1be9e7fab14cb99269d368dd58
Michael Tharp <michael.tharp@sas.com>
2013-09-09 18:06:50 +0000

    ha: automatically resolve split-brain

--- ha/rbuilder.pp
+++ ha/rbuilder.pp
@@ -2,6 +2,7 @@ class { 'rpath_ha':
     service_ip                  => '172.25.0.50',
     multicast_network           => '172.25.0.0',
     multicast_address           => '239.255.1.1',
+    #mount_point                 => '/srv',
     drbd_nodes => [
         {   name                => 'rbuilder-1.mydomain',
             address             => '172.25.0.101:7789',
@@ -13,6 +14,8 @@ class { 'rpath_ha':
             },
         ],
 }
+
+#include rpath_ha::entsrv
 
 service { 'raa':        enable  => false }
 service { 'iptables':   enable  => false }
