357378e55b2a94ac898d960fd28a95091bc20647
Michael Tharp <michael.tharp@sas.com>
2013-11-26 20:10:14 +0000

    puppet: example external project

--- puppet/rbuilder.pp
+++ puppet/rbuilder.pp
@@ -10,6 +10,14 @@ class { 'appengine':
     # sentry_dsn => 'https://xxx:yyy@sentry.example.com/1234',
 }
 
+#rbuilder_project { 'example':
+#    ensure                  => present,
+#    external                => true,
+#    display_name            => "Example External Project",
+#    repository_hostname     => "conary.example.com",
+#    upstream_url            => "https://conary.example.com/conary/",
+#}
+
 
 mailalias { 'root':
     recipient => $admin_email,
