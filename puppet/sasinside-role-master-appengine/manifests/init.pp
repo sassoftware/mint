c3f35224f3f75ac42a4e78233c692bb2cc6fa56a
Mihai Ibanescu <mihai.ibanescu@sas.com>
2014-01-16 11:12:36 -0500

    Added Mw platform, needed for testing (RCE-2551)

--- puppet/sasinside-role-master-appengine/manifests/init.pp
+++ puppet/sasinside-role-master-appengine/manifests/init.pp
@@ -44,4 +44,11 @@ class sasinside-role-master-appengine {
         upstream_url            => $appengine::upstream_url,
     }
 
+    rbuilder_platform { 'vapp.cny.sas.com@sas:vapp-Mw-devel':
+        ensure                  => present,
+        display_name            => "vApp Mw CentOS Devel Workgroup Platform",
+        upstream_url            => $appengine::upstream_url,
+    }
+
+
 }
