7f991330d047695e7579de8eaf4ffe6610d84aae
Michael Tharp <michael.tharp@sas.com>
2015-06-23 19:50:56 +0000

    Remove vapp platform definitions from default puppet modules
    Also remove sasinside-role-master-appengine package and its puppet
    manifest
    
    Change-Id: Iaa9c3af99cd1a9257c8dcfcc410e73c488c044bb

--- puppet/appengine/manifests/platforms.pp
+++ puppet/appengine/manifests/platforms.pp
@@ -1,46 +1,25 @@
 #
 # Copyright (c) SAS Institute Inc.
 #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 class appengine::platforms {
-
-    rbuilder_platform { 'rhel.rpath.com@rpath:rhel-6-server':
-        ensure                  => present,
-        display_name            => 'Red Hat Enterprise Linux 6',
-        upstream_url            => $appengine::upstream_url,
-    }
 
     rbuilder_platform { 'centos6.rpath.com@rpath:centos-6e':
         ensure                  => present,
         display_name            => 'CentOS 6',
         upstream_url            => $appengine::upstream_url,
     }
-
-    #rbuilder_platform { 'windows.rpath.com@rpath:windows-common':
-    #    ensure                  => present,
-    #    display_name            => 'Windows Foundation Platform',
-    #    upstream_url            => $appengine::upstream_url,
-    #    abstract                => true,
-    #}
-
-    rbuilder_platform { 'vapp.cny.sas.com@sas:vapp-2cw-devel':
-        ensure                  => present,
-        display_name            => "vApp 2cw CentOS Devel Workgroup Platform",
-        upstream_url            => $appengine::upstream_url,
-    }
-
-    rbuilder_platform { 'vapp.cny.sas.com@sas:vapp-3w-devel':
-        ensure                  => present,
-        display_name            => "vApp 3w Devel Workgroup Platform",
-        upstream_url            => $appengine::upstream_url,
-    }
-
-    rbuilder_platform { 'vapp.cny.sas.com@sas:vapp-Mw-devel':
-        ensure                  => present,
-        display_name            => "vApp Mw CentOS Devel Workgroup Platform",
-        upstream_url            => $appengine::upstream_url,
-    }
-
-
 
 }
